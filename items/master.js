5eb124a8ceb5c6adbe3e03d06329443d5f699f4a
glowfire <kunchyi1@gmail.com>
2016-07-08 23:53:51 +0800

    Update master.js

--- items/master.js
+++ items/master.js
@@ -1,5 +1,5 @@
 String.prototype.lettersOnly=function(){
-  return this.replace(/[^A-Za-z]/g, "")
+	return this.replace(/[^A-Za-z]/g, "")
 }
 
 String.prototype.numbersOnly=function(){
@@ -17,23 +17,23 @@ String.prototype.getModifiedNameString=function(number){
 Object.prototype.getProductionLevel=function(){
 	var productionLevel = 0;
 	if (this.cookinglevel!==undefined){
-    	productionLevel = this.cookinglevel
-    }
+		productionLevel = this.cookinglevel
+	}
 	if (this.craftinglevel!==undefined){
-    	productionLevel = this.craftinglevel
-    }
+		productionLevel = this.craftinglevel
+    	}
 	if (this.chemistrylevel!==undefined){
-    	productionLevel = this.chemistrylevel
-    }
+		productionLevel = this.chemistrylevel
+    	}
 	if (this.forginglevel!==undefined){
-    	productionLevel = this.forginglevel
-    }
-    return productionLevel
+		productionLevel = this.forginglevel
+    	}
+    	return productionLevel
 }
 
 Object.prototype.getUniqueNameString = function(){
 	var itemProductionLevel = this.getProductionLevel()
-    var itemName = this.name
+	var itemName = this.name
 	return itemName.getModifiedNameString(itemProductionLevel)
 }
 
@@ -44,43 +44,45 @@ normalNameList=[];
 nonUniqueNames=[];
 
 function registerItem(item){
-    var normalName = item.name;
+	var normalName = item.name;
 	var uniqueNumber = item.getProductionLevel();
-    var uniqueName = item.getUniqueNameString();
-    var listedName = normalName;
-    
-    if (normalNameList.indexOf(normalName) > -1){
-    	if (nonUniqueNames.indexOf(normalName)<0){
-        	nonUniqueNames.push(normalName)
-        }
-    }
-    
-    if (item.materials!==undefined){
-    	for (i=0;i<item.materials.length;i++){
-        	var currentMaterialName = item.materials[i].getModifiedNameString("")
-        	if (productMaterialMapping[currentMaterialName]!==undefined){
-            	productMaterialMapping[currentMaterialName].push(item)
-            } else {
-            	productMaterialMapping[currentMaterialName]=[item]
-            }
-        }
-    } else {
-    	if (normalNameList.indexOf(normalName) > -1){
-        	uniqueNumber++
-        	for (j=0;j<normalNameList.length;j++){
-            	if (normalNameList[j]==normalName){
-                	uniqueNumber++
-                }
-            }
-            uniqueName = normalName.getModifiedNameString(uniqueNumber)
-        }
-    }
-    
-    if (normalNameList.indexOf(normalName)>-1){
-    	listedName += " (" + uniqueNumber +")"
-    }
-    
+	var uniqueName = item.getUniqueNameString();
+	var listedName = normalName;
+	
+	if (normalNameList.indexOf(normalName) > -1){
+		if (nonUniqueNames.indexOf(normalName)<0){
+			nonUniqueNames.push(normalName)
+		}
+	}
+	
+	////This part maps products to materials
+	// May have issues with some products needing more than 1 copy of a material, e.g. Twin Leeks
+	if (item.materials!==undefined){
+		for (i=0;i<item.materials.length;i++){
+			var currentMaterialName = item.materials[i].getModifiedNameString("")
+			if (productMaterialMapping[currentMaterialName]!==undefined){
+				productMaterialMapping[currentMaterialName].push(item)
+			} else {
+				productMaterialMapping[currentMaterialName]=[item]
+			}
+		}
+	} else {
+		if (normalNameList.indexOf(normalName) > -1){
+			uniqueNumber++
+			for (j=0;j<normalNameList.length;j++){
+				if (normalNameList[j]==normalName){
+					uniqueNumber++
+				}
+			}
+			uniqueName = normalName.getModifiedNameString(uniqueNumber)
+		}
+	}
+	
+	if (normalNameList.indexOf(normalName)>-1){
+		listedName += " (" + uniqueNumber +")"
+	}
+	
 	masterItemList[uniqueName]=item;
-    masterNameList.push([listedName,normalName,uniqueName,uniqueNumber]);
-    normalNameList.push(normalName)
+	masterNameList.push([listedName,normalName,uniqueName,uniqueNumber]);
+	normalNameList.push(normalName)
 }
