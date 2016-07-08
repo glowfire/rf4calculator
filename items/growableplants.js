9352ed8f8830d6021a556c89214ccbf8cee56599
glowfire <kunchyi1@gmail.com>
2016-07-08 23:38:39 +0800

    Create growableplants.js

---
+++ items/growableplants.js
@@ -0,0 +1,55 @@
+registerItem({
+  "name":"4-Leaf Clover",
+  "category":"Plant",
+  "sell":980,
+  "buy":4000,
+  "pricemultiplier":"E",
+  "effects":{
+    "rp":50
+  },
+  "upgradeinfo":{
+    "rarity":1,//unknown
+    "difficulty":29
+  },
+  "ingredienteffects":{
+    "rp":20//unknown
+  }
+})
+	
+registerItem({
+  "name":"Autumn Grass",
+  "category":"Plant",
+  "sell":1800,
+  "buy":6000,
+  "pricemultiplier":"E",
+  "effects":{
+    "rp":60
+  },
+  "upgradeinfo":{
+    "rarity":1,//unknown
+    "difficulty":35,
+    "slpresp":6
+  },
+  "ingredienteffects":{
+    "rp":20//unknown
+  }
+})
+
+registerItem({
+  "name":"Corn",
+  "category":"Vegetable",
+  "sell":1000,
+  "buy":3000,
+  "pricemultiplier":"E",
+  "effects":{
+    "hp":75
+  },
+  "upgradeinfo":{
+    "rarity":1,
+    "difficulty":1,
+    "vit":6
+  },
+  "ingredienteffects":{
+    "rp":20
+  }
+})
