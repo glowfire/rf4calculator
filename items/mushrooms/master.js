b289731554cd9b9ee3b6fb65bd98ef75fb9fb823
glowfire <kunchyi1@gmail.com>
2016-07-10 02:08:51 +0800

    Update master.js

--- items/mushrooms/master.js
+++ items/mushrooms/master.js
@@ -16,3 +16,22 @@ registerItem({
     "heal":["PSN","SEAL","PAR"]
   }
 })
+
+registerItem({
+  "name":"Mushroom",
+  "category":"Mushroom",
+  "sell":10,
+  "buy":2000,
+  "pricemultiplier":"E",
+  "flavourtext":"Wild mushroom. You would have to test and see if it's safe to eat. Go on. I dare you.",
+  "effects":{ // tentative
+    "hp":500,
+    "rp":100,
+    "heal":["FTG","SICK"]
+  },
+  "cookingeffects":{ // confirmed
+    "hp":500,
+    "rp":100,
+    "heal":["FTG","SICK"]
+  }
+})
