040bd32af2a23843da6a7a6136247ca5baa9c653
glowfire <kunchyi1@gmail.com>
2016-07-10 02:04:58 +0800

    Create master.js

---
+++ items/mushrooms/master.js
@@ -0,0 +1,18 @@
+registerItem({
+  "name":"Mushroom",
+  "category":"Mushroom",
+  "sell":10,
+  "buy":2000,
+  "pricemultiplier":"E",
+  "flavourtext":"Wild mushroom. You would have to test and see if it's safe to eat. Go on. I dare you.",
+  "effects":{
+    "hp":100,
+    "rp":50,
+    "heal":["PSN","SEAL","PAR"]
+  },
+  "cookingeffects":{
+    "hp":100,
+    "rp":50,
+    "heal":["PSN","SEAL","PAR"]
+  }
+})
