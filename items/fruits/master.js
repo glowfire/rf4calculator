17b1516d30d9d68754ea85775ba0a9884593d7e7
glowfire <kunchyi1@gmail.com>
2016-07-09 22:54:15 +0800

    Update master.js

--- items/fruits/master.js
+++ items/fruits/master.js
@@ -1,43 +1,43 @@
 registerItem({
-  "name":"Mealy Apple",
-  "category":"Fruit",
-  "sell":3000,
-  "pricemultiplier":"E",
-  "effects":{
-    "strP":-50,
-    "vitP":-50,
-    "intP":-50,
-    "fireresP":-10,
-    "waterresP":-10,
-    "earthresP":-10,
-    "windresP":-10,
-    "lightresP":-10,
-    "darkresP":-10
-  },
-  "upgradeinfo":{
-    "rarity":15, //confirmed
-    "difficulty":90,
-    "str":-100,
-    "vit":-150,
-    "int":-100,
-    "fireresP":-10,
-    "waterresP":-10,
-    "earthresP":-10,
-    "windresP":-10,
-    "lightresP":-10,
-    "darkresP":-10
-  },
-  "cookingeffects":{
-    "strP":-5,
-    "vitP":-5,
-    "intP":-5,
-    "fireresP":-10,
-    "waterresP":-10,
-    "earthresP":-10,
-    "windresP":-10,
-    "lightresP":-10,
-    "darkresP":-10
-  }
+	"name":"Mealy Apple",
+	"category":"Fruit",
+	"sell":3000,
+	"pricemultiplier":"E",
+	"effects":{
+		"strP":-50,
+		"vitP":-50,
+		"intP":-50,
+		"fireresP":-10,
+		"waterresP":-10,
+		"earthresP":-10,
+		"windresP":-10,
+		"lightresP":-10,
+		"darkresP":-10
+	},
+	"upgradeinfo":{
+		"rarity":15, //confirmed
+		"difficulty":90,
+		"str":-100,
+		"vit":-150,
+		"int":-100,
+		"fireresP":-10,
+		"waterresP":-10,
+		"earthresP":-10,
+		"windresP":-10,
+		"lightresP":-10,
+		"darkresP":-10
+	},
+	"cookingeffects":{
+		"strP":-5,
+		"vitP":-5,
+		"intP":-5,
+		"fireresP":-10,
+		"waterresP":-10,
+		"earthresP":-10,
+		"windresP":-10,
+		"lightresP":-10,
+		"darkresP":-10
+	}
 })
 
 registerItem({
@@ -47,20 +47,20 @@ registerItem({
 	"buy":800,
 	"pricemultiplier":"E",
 	"effects":{
-	  "hp":35,
-	  "rp":15
+		"hp":35,
+		"rp":15
 	},
 	"upgradeinfo":{
-	  "rarity":4,
-	  "difficulty":24,
-	  "str":3,
-	  "vit":3
+		"rarity":4,
+		"difficulty":24,
+		"str":3,
+		"vit":3
 	},
 	"ingredienteffects":{ // confirmed
-    	"hp":35,
-    	"rp":15, // wikia is wrong
-    	"psnresP":5,
-    	"sealresP":5,
-    	"parresP":5
-  }
+	"hp":35,
+	"rp":15, // wikia is wrong
+	"psnresP":5,
+	"sealresP":5,
+	"parresP":5
+	}
 })
