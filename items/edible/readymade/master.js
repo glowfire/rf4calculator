d58adce90ea93a3b0a834e00195e99a272ec70ca
glowfire <kunchyi1@gmail.com>
2016-07-10 18:34:04 +0800

    Create master.js

---
+++ items/edible/readymade/master.js
@@ -0,0 +1,13 @@
+registerItem({
+"name":"Chocolate",
+"category":"Ready-made",
+"sell":150,
+"buy":560,
+"flavourtext":"???",
+"pricemultiplier":"Ch",
+"effects":{
+"rp":15
+},
+"ingredienteffects":{
+"rp":20// unkwnon
+})
