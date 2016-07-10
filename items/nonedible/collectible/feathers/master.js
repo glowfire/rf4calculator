8afa0d6b91c7fe7a1a8c579eba1642ecd55dcaa0
glowfire <kunchyi1@gmail.com>
2016-07-10 22:39:56 +0800

    Create master.js

---
+++ items/nonedible/collectible/feathers/master.js
@@ -0,0 +1,72 @@
+
+
+registerItem({
+	"name":"Bird's Feather",
+	"category":"Feathers",
+	"sell":31,
+	"buy":155,
+	"flavourtext":"A soft feather that's prickly at the base and pointy on one end.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":2,
+		"difficulty":10,
+		"int":2
+	}
+});
+
+registerItem({
+	"name":"Yellow Feather",
+	"category":"Feathers",
+	"sell":100,
+	"buy":800,
+	"flavourtext":"A soft-textured yellow feather. It's believed to bring happiness.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":5,
+		"difficulty":90,
+		"int":130
+	}
+});
+
+registerItem({
+	"name":"Black Bird Feather",
+	"category":"Feathers",
+	"sell":135,
+	"buy":1080,
+	"flavourtext":"A soft black feather. The quill is rather sharp.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":10,
+		"difficulty":50,
+		"int":14
+	}
+});
+
+registerItem({
+	"name":"Thunderbird Feather",
+	"category":"Feathers",
+	"sell":210,
+	"buy":1680,
+	"flavourtext":"A soft-textured feather that contains electricity.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":12,
+		"difficulty":58,
+		"int":28
+	}
+});
+
+registerItem({
+	"name":"Dragon Fin",
+	"category":"Feathers",
+	"sell":1e4,
+	"buy":3e5,
+	"flavourtext":"The fin of a legendary dragon. A perfect material that's hard, supple and lightweight.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":15,
+		"difficulty":96,
+		"vit":-10,
+		"int":300
+	}
+});
