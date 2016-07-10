9f0863508916bd486f13a9369c26077f86208703
glowfire <kunchyi1@gmail.com>
2016-07-10 23:13:32 +0800

    Create master.js

---
+++ items/nonedible/collectible/shellsandbones/master.js
@@ -0,0 +1,94 @@
+
+
+registerItem({
+	"name":"Turtle Shell",
+	"category":"Shells and Bones",
+	"sell":30,
+	"buy":150,
+	"flavourtext":"A hard turtle shell. Good when you want to make something hard. (Hey, get your mind out of the gutter!)",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":2,
+		"difficulty":15,
+		"def":4,
+		"mdef":1,
+		"critP":-3
+	}
+});
+
+registerItem({
+	"name":"Fish Fossil",
+	"category":"Shells and Bones",
+	"sell":33,
+	"buy":165,
+	"flavourtext":"An ancient fish fossil. Some collectors would kill to find one!",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":3,
+		"difficulty":20,
+		"matk":4,
+		"mdef":1
+	}
+});
+
+registerItem({
+	"name":"Dragon Bones",
+	"category":"Shells and Bones",
+	"sell":1800,
+	"buy":24000,
+	"flavourtext":"Ancient dragon bones. Old, but still quite tough. Used for numerous purposes.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":8,
+		"difficulty":50,
+		"def":10,
+		"mdef":10,
+		"vit":10
+	}
+});
+
+registerItem({
+	"name":"Skull",
+	"category":"Shells and Bones",
+	"sell":120,
+	"buy":960,
+	"flavourtext":"A creepy skull. Some people love this kind of stuff, though.",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":9,
+		"difficulty":39,
+		"matk":8,
+		"mdef":5,
+		"sickatkP":4
+	}
+});
+
+registerItem({////////////////////////////////////////////////////////////////////
+	"name":"Blk",
+	"category":"Shells and Bones",
+	"sell":33,
+	"buy":165,
+	"flavourtext":"An ancient fish fossil. Some collectors would kill to find one!",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":3,
+		"difficulty":20,
+		"matk":4,
+		"mdef":1
+	}
+});
+
+registerItem({
+	"name":"Ammonite",
+	"category":"Shells and Bones",
+	"sell":33,
+	"buy":165,
+	"flavourtext":"An ancient fish fossil. Some collectors would kill to find one!",
+	"pricemultiplier":"E",
+	"upgradematerial":{
+		"rarity":3,
+		"difficulty":20,
+		"matk":4,
+		"mdef":1
+	}
+});
