811a8a066a3b54400aec73427793dadf137cf3dd
glowfire <kunchyi1@gmail.com>
2016-07-10 21:49:17 +0800

    Create master.js

---
+++ items/nonedible/collectible/liquids/master.js
@@ -0,0 +1,15 @@
+
+
+registerItem({
+"name":"Devil Blood",
+"category":"Liquids",
+"sell":116,
+"buy":920,
+"flavourtext":"Life's blood collected from demons. Valued as medicine.",
+"pricemultiplier":"E",
+"upgradematerial":{"rarity":9,
+"difficulty":39,
+"matk":9,
+"drainresP":15},
+"cookingeffects":{ // confirmed
+"rpmax":1}});
