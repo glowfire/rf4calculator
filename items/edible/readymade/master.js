849e7a3018985a4c467e83ac2667645c8e81d8dc
glowfire <kunchyi1@gmail.com>
2016-07-10 20:04:56 +0800

    Update master.js

--- items/edible/readymade/master.js
+++ items/edible/readymade/master.js
@@ -1,13 +1,15 @@
 registerItem({
-"name":"Chocolate",
-"category":"Ready-made",
-"sell":150,
-"buy":560,
-"flavourtext":"???",
-"pricemultiplier":"Ch",
-"effects":{
-"rp":15
-},
-"ingredienteffects":{
-"rp":20// unkwnon
-})
+	"name":"Chocolate",
+	"category":"Ready-made",
+	"sell":150,
+	"buy":400,
+	"flavourtext":"Smooth and sweet, it's an absolute standard. Found in endless varieties.",
+	"pricemultiplier":"Ch",
+	"effects":{
+	  "rp":10
+	},
+	"cookingeffects":{
+	  "rp":10,
+	  "int":2
+	}
+});
