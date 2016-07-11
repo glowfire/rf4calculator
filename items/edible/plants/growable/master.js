b637fe298d562392f70a41f1163c4b5e5cb75fcc
glowfire <kunchyi1@gmail.com>
2016-07-11 20:09:07 +0800

    Update master.js

--- items/edible/plants/growable/master.js
+++ items/edible/plants/growable/master.js
@@ -1,36 +1,40 @@
 registerItem({
-  "name":"4-Leaf Clover",
-  "category":"Plant",
-  "sell":980,
-  "buy":4000,
-  "pricemultiplier":"E",
-  "effects":{
-    "rp":50
-  },
-  "upgradeinfo":{
-    "rarity":1,//unknown
-    "difficulty":29
-  },
-  "ingredienteffects":{
-    "rp":20//unknown
-  }
+	"name":"4-Leaf Clover",
+	"category":"Plant",
+	"sell":980,
+	"buy":4000,
+	"pricemultiplier":"E",
+	"flavourtext":"A plant which brings you good luck. Carry it around or make it a gift, and happiness will follow you!",
+	"effects":{
+		"rp":50
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":29
+	},
+	"cookingeffects":{
+		"rp":50,
+		"critP":5
+	}
 })
 	
 registerItem({
-  "name":"Autumn Grass",
-  "category":"Plant",
-  "sell":1800,
-  "buy":6000,
-  "pricemultiplier":"E",
-  "effects":{
-    "rp":60
-  },
-  "upgradeinfo":{
-    "rarity":1,//unknown
-    "difficulty":35,
-    "slpresp":6
-  },
-  "ingredienteffects":{
-    "rp":20//unknown
-  }
+	"name":"Autumn Grass",
+	"category":"Plant",
+	"sell":1800,
+	"buy":6000,
+	"pricemultiplier":"E",
+	"flavourtext":"A plant which grows colorful flowers. Named as it is due to its autumnal color scheme.",
+	"effects":{
+		"rp":60
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":35,
+		"slpresP":6
+	},
+	"ingredienteffects":{
+		"rp":60,
+		"rpmaxP":1
+	}
 })
