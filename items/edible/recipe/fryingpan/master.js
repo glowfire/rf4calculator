7c4a04ad5020c26aa508b6be336af7e2d2a7c546
glowfire <kunchyi1@gmail.com>
2016-07-12 23:38:22 +0800

    Create master.js

---
+++ items/edible/recipe/fryingpan/master.js
@@ -0,0 +1,40 @@
+
+
+registerItem({
+	"name":"Miso Eggplant",
+	"category":"Frying Pan",
+	"cookinglevel":31,
+	"sell":375,
+	"buy":1800,,
+	"pricemultiplier":"C1",
+	"flavourtext":"Roasted eggplant with miso. Mellow and rich in flavor.",
+	"effects":{
+		"hp":4200,
+		"hpmax":180,
+		"str":13,
+		"vit":13,
+		"int":13
+	},
+	"materials":["Eggplant"]
+})
+
+registerItem({
+	"name":"Popcorn",
+	"category":"Frying Pan",
+	"cookinglevel":51,
+	"sell":1800,
+	"buy":6200,,
+	"pricemultiplier":"C1",
+	"flavourtext":"Corn kernels popped in a pan. Generally eaten with salt.",
+	"effects":{
+		"hp":5000,
+		"hpP":14,
+		"rp":1000,
+		"hpmax":600,
+		"str":40,
+		"vit":40,
+		"int":40
+	},
+	"materials":["Corn"]
+})
+	
