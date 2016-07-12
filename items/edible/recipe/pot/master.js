9a389a4d62c3354702b93ad8f696acac43412712
glowfire <kunchyi1@gmail.com>
2016-07-12 23:55:10 +0800

    Create master.js

---
+++ items/edible/recipe/pot/master.js
@@ -0,0 +1,16 @@
+
+registerItem({
+	"name":"Boiled Spinach",
+	"cookinglevel":13,
+	"category":"Pot",
+	"sell":95,
+	"buy":340,
+	"pricemultiplier":"C1",
+	"flavourtext":"Boiled and flavored spinach. You must be careful not to over-boil it.",
+	"effects":{
+		"hp":800,
+		"rp":360,
+		"hpmax":300
+	},
+	"materials":["Spinach"]
+})
