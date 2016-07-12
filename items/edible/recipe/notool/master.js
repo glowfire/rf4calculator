d35674d628e5a1d5eb98429940bbffb526ff29d7
glowfire <kunchyi1@gmail.com>
2016-07-12 23:21:41 +0800

    Update master.js

--- items/edible/recipe/notool/master.js
+++ items/edible/recipe/notool/master.js
@@ -2,105 +2,105 @@
 
 registerItem({
 	"name":"Pickled Turnip",
-    "cookinglevel":4,
+	"cookinglevel":4,
 	"category":"No Tool",
-    "sell":48,
-    "buy":400,
-    "pricemultiplier":"C1",
-    "flavourtext":"Turnip that's been pickled. Has a fresh taste and is sometimes eaten after meals.",
-    "effects":{
-    	"hp":250,
-        "rp":100,
-        "str":5,
-        "vit":3,
-        "int":5
-        },
+	"sell":48,
+	"buy":400,
+	"pricemultiplier":"C1",
+	"flavourtext":"Turnip that's been pickled. Has a fresh taste and is sometimes eaten after meals.",
+	"effects":{
+		"hp":250,
+		"rp":100,
+		"str":5,
+		"vit":3,
+		"int":5
+	},
 	"materials":["Turnip"]
 })
 
 registerItem({
 	"name":"Pickles",
-    "cookinglevel":10,
+	"cookinglevel":10,
 	"category":"No Tool",
-    "sell":105,
-    "buy":450,
-    "pricemultiplier":"C1",
-    "flavourtext":"Lightly pickled cucumber. Has a crunchy texture.",
-    "effects":{
-    	"hp":400,
-        "rp":100,
-        "str":4,
-        "vit":3,
-        "int":2
-        },
+	"sell":105,
+	"buy":450,
+	"pricemultiplier":"C1",
+	"flavourtext":"Lightly pickled cucumber. Has a crunchy texture.",
+	"effects":{
+		"hp":400,
+		"rp":100,
+		"str":4,
+		"vit":3,
+		"int":2
+	},
 	"materials":["Cucumber"]
 })
 
 registerItem({
 	"name":"Pickled Turnip",
-    "cookinglevel":12,
+	"cookinglevel":12,
 	"category":"No Tool",
-    "sell":48,
-    "buy":400,
-    "pricemultiplier":"C1",
-    "flavourtext":"Turnip that's been pickled. Has a fresh taste and is sometimes eaten after meals.",
-    "effects":{
-    	"hp":250,
-        "rp":100,
-        "str":5,
-        "vit":3,
-        "int":5
-        },
+	"sell":48,
+	"buy":400,
+	"pricemultiplier":"C1",
+	"flavourtext":"Turnip that's been pickled. Has a fresh taste and is sometimes eaten after meals.",
+	"effects":{
+		"hp":250,
+		"rp":100,
+		"str":5,
+		"vit":3,
+		"int":5
+	},
 	"materials":["Pink Turnip"]
 })
 
 registerItem({
 	"name":"Pickle Mix",
-    "cookinglevel":48,
+	"cookinglevel":48,
 	"category":"No Tool",
-    "sell":520,
-    "buy":2800,
-    "pricemultiplier":"C1",
-    "flavourtext":"Made with spring and winter turnips. The red and white colors symbolize good luck. Often served at festivals.",
-    "effects":{
-    	"hp":5000,
-        "hpP":11,
-        "rp":1000,
-        "str":60,
-        "strP":3,
-        "vit":20,
-        "vitP":3,
-        "int":35,
-        "intP":1,
-        "fireresP":10,
-        "lightresP":10
+	"sell":520,
+	"buy":2800,
+	"pricemultiplier":"C1",
+	"flavourtext":"Made with spring and winter turnips. The red and white colors symbolize good luck. Often served at festivals.",
+	"effects":{
+		"hp":5000,
+		"hpP":11,
+		"rp":1000,
+		"str":60,
+		"strP":3,
+		"vit":20,
+		"vitP":3,
+		"int":35,
+		"intP":1,
+		"fireresP":10,
+		"lightresP":10
         },
-	"materials":["Turnip","Pink Turnip"]
+        "materials":["Turnip","Pink Turnip"]
 })
 
 registerItem({
 	"name":"Turnip Heaven",
-    "cookinglevel":96,
+	"cookinglevel":96,
 	"category":"No Tool",
-    "sell":150000,
-    "buy":542000,
-    "pricemultiplier":"C1",
-    "flavourtext":"Turnips from three corners of the globe, gathered together into a turnip paradise. Try to eat all three at once!",
-    "effects":{
-    	"hp":5000,
-        "hpP":90,
-        "rp":1000,
-        "rpP":90,
-        "str":90,
-        "strP":12,
-        "vit":80,
-        "vitP":15,
-        "int":50,
-        "intP":10,
-        "fireresP":20,
-        "lightresP":20,
-        "darkresP":20
+	"sell":150000,
+	"buy":542000,
+	"pricemultiplier":"C1",
+	"flavourtext":"Turnips from three corners of the globe, gathered together into a turnip paradise. Try to eat all three at once!",
+	"effects":{
+		"hp":5000,
+		"hpP":90,
+		"rp":1000,
+		"rpP":90,
+		"str":90,
+		"strP":12,
+		"vit":80,
+		"vitP":15,
+		"int":50,
+		"intP":10,
+		"fireresP":20,
+		"lightresP":20,
+		"darkresP":20
         },
-	"materials":["Turnip","Pink Turnip","Golden Turnip"]
+        "materials":["Turnip","Pink Turnip","Golden Turnip"]
 })
 
