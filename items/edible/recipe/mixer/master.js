b9cd69b890ba19a96a4588401a71f1a5003c2a88
glowfire <kunchyi1@gmail.com>
2016-07-13 18:13:08 +0800

    Update master.js

--- items/edible/recipe/mixer/master.js
+++ items/edible/recipe/mixer/master.js
@@ -16,3 +16,21 @@ registerItem({
 	"flavourtext":"Juice made from fresh tomatoes. Thick and highly nutritious.",
 	"pricemultiplier":"C1"
 })
+
+registerItem({
+	"name":"Vegetable Juice"
+	"cookinglevel":59,
+	"materials":["Pumpkin","Turnip","Carrot","Spinach"],
+	"category":"Mixer",
+	"sell":2350,
+	"buy":8000,
+	"effects":{
+		"hp":5000,
+		"hpP":59,
+		"rp":1000,
+		"int":80,
+		"intP":5
+	}
+	"flavourtext":"A healthy juice made from various vegetables. Bitter but strangely addictive.",
+	"pricemultiplier":"C1"
+})
