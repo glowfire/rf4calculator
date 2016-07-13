0e0c48ab53ce964bf92e86499c3ecfcfc1bfda40
glowfire <kunchyi1@gmail.com>
2016-07-13 17:44:42 +0800

    Create master.js

---
+++ items/edible/recipe/oven/master.js
@@ -0,0 +1,34 @@
+
+
+registerItem({
+	"name":"Sweet Potato",
+	"cookinglevel":20,
+	"category":"Oven",
+	"materials":["Yam"],
+	"sell":240,
+	"buy":870,
+	"pricemultiplier":"C1",
+	"flavourtext":"A slow-roasted sweet potato. Often cooked over bonfires in the fall."
+	"effects":{
+		"hp":2020,
+		"rp":500,
+		"vit":10
+	}
+})
+
+registerItem({
+	"name":"Corn on the Cob",
+	"cookinglevel":52,
+	"category":"Oven",
+	"materials":["Corn"],
+	"sell":1860,
+	"buy":6200,
+	"pricemultiplier":"C1",
+	"flavourtext":"Fragrantly roasted corn. Very sweet."
+	"effects":{
+		"hp":5000,
+		"hpP":20,
+		"rp":1000,
+		"vit":180
+	}
+})
