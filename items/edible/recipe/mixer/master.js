7fe1eb80330a94b35455927af9998c4bbc92d407
glowfire <kunchyi1@gmail.com>
2016-07-13 18:09:57 +0800

    Create master.js

---
+++ items/edible/recipe/mixer/master.js
@@ -0,0 +1,18 @@
+
+
+registerItem({
+	"name":"Tomato Juice"
+	"cookinglevel":35,
+	"materials":["Tomato"],
+	"category":"Mixer",
+	"sell":1e3,
+	"buy":3200,
+	"effects":{
+		"heal":["FTG"],
+		"hp":4000,
+		"rp":920,
+		"int":45
+	}
+	"flavourtext":"Juice made from fresh tomatoes. Thick and highly nutritious.",
+	"pricemultiplier":"C1"
+})
