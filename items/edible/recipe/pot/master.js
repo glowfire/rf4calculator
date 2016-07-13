632da6230b6301478b8babf178d651ecac8464ed
glowfire <kunchyi1@gmail.com>
2016-07-13 09:55:01 +0800

    Update master.js

--- items/edible/recipe/pot/master.js
+++ items/edible/recipe/pot/master.js
@@ -30,3 +30,34 @@ registerItem({
 	"flavourtext":"Flavored and boiled pumpkin. Soft in texture. Boiling brings out the natural sweetness of the pumpkin.",
 	"pricemultiplier":"C1"
 })
+
+registerItem({
+	"name":"Strawberry Jam",
+	"cookinglevel":27,
+	"materials":["Strawberry"],
+	"category":"Pot",
+	"sell":215,
+	"buy":680,
+	"effects":{
+		"hp":30,
+		"rp":10
+	},
+	"flavourtext":"Jam made by boiling strawberries. Generally eaten with bread.",
+	"pricemultiplier":"C1"
+})
+
+registerItem({
+	"name":"Grilled Miso",
+	"cookinglevel":29,
+	"materials":["Radish"],
+	"category":"Pot",
+	"sell":660,
+	"buy":2000,
+	"effects":{
+		"hp":3000,
+		"rp":880,
+		"hpmax":1500
+	},
+	"flavourtext":"Boiled and flavored with radish. Very mild and good for digestion.",
+	"pricemultiplier":"C1"
+})
