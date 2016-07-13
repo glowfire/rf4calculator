45b4988abe880049cf67d968a042a4c0c048e410
glowfire <kunchyi1@gmail.com>
2016-07-13 23:20:47 +0800

    Update master.js

--- items/edible/sansrecipe/ingredients/master.js
+++ items/edible/sansrecipe/ingredients/master.js
@@ -20,7 +20,7 @@ registerItem({
 	"category":"Ingredient",
 	"sell":250,
 	"buy":650,
-	"effects":{
+	"effects":{////////////////////////// different effect multiplier than standard. May be related to the [readymade] items.
 		"fireresP":-30
 	},
 	"cookingeffects":{
@@ -28,5 +28,35 @@ registerItem({
 		"fireresP":-30
 	},
 	"flavourtext":"Often used in fried recipes to add flavor and prevent sticking. Careful, it's highly flammable.",
+	"pricemultiplier":"Sd"
+})
+
+registerItem({
+	"name":"Curry Powder",
+	"category":"Ingredient",
+	"sell":70,
+	"buy":300,
+	"effects":{
+	},
+	"cookingeffects":{///////////// unknown
+		"rp":5,
+		"str":1
+	},
+	"flavourtext":"Powder used to cook curry. Made by mixing various spices. Goes well with lots of stuff.",
+	"pricemultiplier":"Sd"
+})
+
+registerItem({
+	"name":"Rice Flour",
+	"category":"Ingredient",
+	"sell":40,
+	"buy":100,
+	"effects":{
+	},
+	"cookingeffects":{///////////// unknown
+		"rp":5,
+		"str":1
+	},
+	"flavourtext":"Powder used to make rice dumplings. Adds stickiness to the resulting dish.",
 	"pricemultiplier":"Sd"
 })
