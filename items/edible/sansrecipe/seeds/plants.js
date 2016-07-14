08e54a4a3a6829188b6b45b25f39dd48928673bf
glowfire <kunchyi1@gmail.com>
2016-07-15 01:07:59 +0800

    Create plants.js

---
+++ items/edible/sansrecipe/seeds/plants.js
@@ -0,0 +1,46 @@
+// Toyherb, Pink Cat, Moondrop, Charm Blue, Cherry Grass, Pom-Pom Grass, Lamp Grass, Ironleaf, Clover, Autumn Grass, Noel Grass,
+// Fireflower, Emery Flower, Blue, Green, Red, White Crystal, in that order.
+
+registerItem({
+	"name":"Toyherb Seeds",
+	"sell":6,
+	"buy":60,
+	"value":65,
+	"growth":4,
+	"harvested":2,
+	"flavourtext":"Grows well in spring but not in winter. A fast grower, but delicate and tough to harvest. Cures fatigue if fully grown.",
+	"regrow":false,
+	"seasons":{
+		"spring":"good",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Seeds",
+	"sell":,
+	"buy":0,
+	"value":,
+	"growth":,
+	"harvested":2,
+	"flavourtext":"Grows well in spring but not in winter.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
