5f6a2770f1560a845e33bf39ed9f65b49c91a5a0
glowfire <kunchyi1@gmail.com>
2016-07-15 13:34:36 +0800

    Update other.js

--- scripts/other.js
+++ scripts/other.js
@@ -1,54 +1,5 @@
 // Rewrite these.
 
-function createMaterialListing(){
-
-	currentMaterialsBuyPrice = 0
-    currentMaterialsBuyPriceDiscount = 0
-
-	if (currentItem.materials!==undefined){
-		var materialsList = document.createElement('p');
-		var materialsListString = "Materials: "
-		var node = document.createTextNode(materialsListString);
-		materialsList.appendChild(node);
-    	for (j=0;j<currentItem.materials.length;j++){
-    		var currentMaterial = currentItem.materials[j].convertStringToItem()
-    		materialsList.innerHTML+="<br>"+(j+1)+". "+currentMaterial.name
-			if (currentMaterial.buy!==undefined){
-        		currentMaterialBuy=currentMaterial.buy;
-				currentMaterialsBuyPrice+=currentMaterialBuy;
-    			currentMaterialsBuyPriceDiscount+=Math.ceil(currentMaterialBuy*.9);
-    			materialsList.innerHTML+=" (buy: "+currentMaterial.buy+" G, discounted: "
-        		materialsList.innerHTML+=Math.ceil(currentMaterial.buy*.9)+" G)"
-        	} else {
-    			materialsList.innerHTML+=" (cannot be bought)"
-        	}
-    	}
-    
-    	if (currentMaterialsBuyPrice>0){
-			materialsList.innerHTML+="<br><br>Total cost of buyable materials: "+currentMaterialsBuyPrice+" G "
-			materialsList.innerHTML+="(all discounted: "+currentMaterialsBuyPriceDiscount+" G)"
-        
-        	if (currentItem.buy==undefined){
-        		materialsList.innerHTML+="<br>This item cannot be bought and must be produced."
-        	} else if (allMaterialsBuyable(currentItem.materials)){
-            	if (currentMaterialsBuyPrice>currentItem.buy){
-        			materialsList.innerHTML+="<br>Assuming no discounts, it is cheaper to "
-            		materialsList.innerHTML+="<span style='font-weight:bold'>buy the product directly</span>";
-            		materialsList.innerHTML+=" than buying and processing the materials."
-        		} else {
-        			materialsList.innerHTML+="<br>Assuming no discounts, it is cheaper to "
-            		materialsList.innerHTML+="<span style='font-weight:bold'>buy and process the materials</span>";
-            		materialsList.innerHTML+=" than buying the product directly."
-				}
-        	}
-        
-    	} else {
-    		materialsList.innerHTML+="<br><br>None of the materials can be bought."
-    	}
-    }
-    document.getElementById('currentItemDiv').appendChild(materialsList)
-}
-
 // Section for level independent stats like upgrade info
 var independentStats
 
