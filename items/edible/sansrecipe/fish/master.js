ab1794123398428660193899a4c5faa4221495f6
glowfire <kunchyi1@gmail.com>
2016-07-15 23:49:55 +0800

    Create master.js

---
+++ items/edible/sansrecipe/fish/master.js
@@ -0,0 +1,353 @@
+// Follow reverse order of listing on shipping record
+
+registerItem({
+	"name":"Masu Trout",
+	"category":"Fish",
+	"sell":15,
+	"buy":300,
+	"flavourtext":"One of the most common fish. Mostly found in rivers, but sometimes also found out at sea.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Squid",
+	"category":"Fish",
+	"sell":24,
+	"buy":400,
+	"flavourtext":"A mollusk that's active at night. Uses the fins on its body to swim like it's flying in the water.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Taimen",
+	"category":"Fish",
+	"sell":6500,
+	"buy":78000,
+	"flavourtext":"The largest freshwater salmonid. Very rare and valued.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Sardine",
+	"category":"Fish",
+	"sell":70,
+	"buy":1200,
+	"flavourtext":"A fairly easy fish to catch. Swims in shoals, and is often viewed as a beautiful, tasty delight.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Char",
+	"category":"Fish",
+	"sell":92,
+	"buy":1800,
+	"flavourtext":"A fish people just love to catch. Prefers swimming in cold water.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Chub",
+	"category":"Fish",
+	"sell":130,
+	"buy":2600,
+	"flavourtext":"A fish with a dark line on its body. During spawning season, it develops two more lines.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Glitter Snapper",
+	"category":"Fish",
+	"sell":8800,
+	"buy":12e4,
+	"flavourtext":"A fish with a body of glittering silver. Very easy to spot, but hard to catch.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Skipjack",
+	"category":"Fish",
+	"sell":220,
+	"buy":4500,
+	"flavourtext":"A fall fish that requires power and skill to catch.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Turbot",
+	"category":"Fish",
+	"sell":120,
+	"buy":2500,
+	"flavourtext":"A fish with both eyes situated on the right side of its body. Changes color depending on its environment.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Gibelio",
+	"category":"Fish",
+	"sell":100,
+	"buy":2e3,
+	"flavourtext":"Displays unique breeding characteristics due to females outnumbering males. Found in quiet rivers.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Salmon",
+	"category":"Fish",
+	"sell":80,
+	"buy":1350,
+	"flavourtext":"Spends most of its time in the ocean, but swims upstream to spawn in the river. Used in many recipes.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Mackerel",
+	"category":"Fish",
+	"sell":60,
+	"buy":1e3,
+	"flavourtext":"Spoils easily. Often grilled or boiled, and often for a long time in order to disguise that it has spoiled. Tsk tsk!",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Needlefish",
+	"category":"Fish",
+	"sell":30,
+	"buy":500,
+	"flavourtext":"Swims near the water's surface with its tiny, thin body. Has an aqua-colored back and a white belly.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Pike",
+	"category":"Fish",
+	"sell":12,
+	"buy":240,
+	"flavourtext":"Named after its slim, long and slender body. A popular fall fish that's full of nutrients.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Sunsquid",
+	"category":"Fish",
+	"sell":6800,
+	"buy":74e3,
+	"flavourtext":"A squid from the tropics. Has a tanned body and swims in shallow water.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Shrimp",
+	"category":"Fish",
+	"sell":85,
+	"buy":1500,
+	"flavourtext":"A type of crustacean with its body protected by a hard shell. Has 13 pairs of legs.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Snapper",
+	"category":"Fish",
+	"sell":1280,
+	"buy":14800,
+	"flavourtext":"A fish known to bring good luck. Has long been served in festivals and ceremonies.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Throbby Snapper",
+	"category":"Fish",
+	"sell":3500,
+	"buy":38e3,
+	"flavourtext":"A rare type of snapper that's considered legendary among fishermen.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Rainbow Trout",
+	"category":"Fish",
+	"sell":150,
+	"buy":3e3,
+	"flavourtext":"A mid-sized fish that lives in rivers. Very poisonous. During spawning season, the males shine in rainbow colors.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Flounder",
+	"category":"Fish",
+	"sell":190,
+	"buy":4e3,
+	"flavourtext":"A fish with both eyes on the left side of its body. Has a larger mouth and sharper teeth than a Turbot.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Blowfish",
+	"category":"Fish",
+	"sell":4200,
+	"buy":57e3,
+	"flavourtext":"Known as one of the most expensive fish. Filled with poison, requiring high skill to cook safely.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Yellowtail",
+	"category":"Fish",
+	"sell":3800,
+	"buy":45e3,
+	"flavourtext":"A stock name given to numerous varieties of fish. All have large bodies and are renowned for bringing good luck.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Crucian Carp",
+	"category":"Fish",
+	"sell":35,
+	"buy":620,
+	"flavourtext":"Matures fast, and also lives long. Mostly eats plankton. Requires both skill and luck to catch.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Tuna",
+	"category":"Fish",
+	"sell":5600,
+	"buy":61e3,
+	"flavourtext":"A large and tasty fish that's widely loved. Traded at high prices and generally found in deep waters.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Girella",
+	"category":"Fish",
+	"sell":45,
+	"buy":850,
+	"flavourtext":"A winter fish caught in the ocean. Easy to find, but renowned for its powerful pull.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Fall Flounder",
+	"category":"Fish",
+	"sell":2e3,
+	"buy":27e3,
+	"flavourtext":"A fish that looks like fallen leaves. When seen swimming, you'd just think there's debris floating by.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Cherry Salmon",
+	"category":"Fish",
+	"sell":18,
+	"buy":350,
+	"flavourtext":"A small freshwater fish. Found in various places due to its popularity -- it's often <q>stocked.</q>",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Lamp Squid",
+	"category":"Fish",
+	"sell":1800,
+	"buy":21e3,
+	"flavourtext":"A squid with a lamp-shaped body. The lamp holds poison with a sealing effect, so it must be cooked with caution.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Lobster",
+	"category":"Fish",
+	"sell":960,
+	"buy":9600,
+	"flavourtext":"A giant shrimp with walking legs. Its two large front claws are used to threaten enemies.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Pond Smelt",
+	"category":"Fish",
+	"sell":10,
+	"buy":200,
+	"flavourtext":"Thin and small, but adaptive, with a large habitat. Most often found during ice fishing.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Sand Flounder",
+	"category":"Fish",
+	"sell":38,
+	"buy":700,
+	"flavourtext":"A unique fish that swims in the sand. Gets nutrition from small minerals, making its body firm and sandy.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
+
+registerItem({
+	"name":"Rockfish",
+	"category":"Fish",
+	"sell":1040,
+	"buy":10400,
+	"flavourtext":"A unique fish that swims in the sand. Has a hard head that can crush even rocks.",
+	"pricemultiplier":"C1",
+	"cookingeffects":{
+	}
+})
