992156eea0118ce46de8ca1b593c2aea669e133e
glowfire <kunchyi1@gmail.com>
2016-07-16 00:11:26 +0800

    Update master.js

--- items/edible/recipe/oven/master.js
+++ items/edible/recipe/oven/master.js
@@ -1,6 +1,24 @@
 
 
 registerItem({
+	"name":"Bread",
+	"cookinglevel":8,
+	"category":"Oven",
+	"materials":["Flour"],
+	"sell":145,
+	"buy":440,
+	"pricemultiplier":"C1",
+	"flavourtext":"A staple food for many. You can toast it, stuff it, stick things between it or eat it any other way you'd like.",
+	"effects":{
+		"hp":400,
+		"rp":120,
+		"str":4,
+		"vit":4,
+		"int":4
+	}
+})
+
+registerItem({
 	"name":"Sweet Potato",
 	"cookinglevel":20,
 	"category":"Oven",
@@ -13,6 +31,22 @@ registerItem({
 		"hp":2020,
 		"rp":500,
 		"vit":10
+	}
+})
+
+registerItem({
+	"name":"Toast",
+	"cookinglevel":21,
+	"category":"Oven",
+	"materials":["Bread"],
+	"sell":170,
+	"buy":610,
+	"pricemultiplier":"C1",
+	"flavourtext":"Browned bread. Neck-and-neck with rice in the struggle for worldwide breakfast dominance.",
+	"effects":{
+		"hp":2040,
+		"rp":800,
+		"vit":15
 	}
 })
 
