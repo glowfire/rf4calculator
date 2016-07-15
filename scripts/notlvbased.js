d102cda252e527acbd85578f8d83568bbf11b25a
glowfire <kunchyi1@gmail.com>
2016-07-15 13:13:27 +0800

    Update notlvbased.js

--- scripts/notlvbased.js
+++ scripts/notlvbased.js
@@ -1,1 +1,52 @@
 //
+
+function showBasicStats(){
+	var nameLine = document.createElement('h1');
+	var nameString = currentItem.name;
+	nameLine.appendChild(document.createTextNode(nameString));
+	nameLine.style.fontWeight="bold";
+	document.getElementById('currentItemDiv').appendChild(nameLine);
+	
+	var flavourTextLine = document.createElement('p');
+	document.getElementById('currentItemDiv').appendChild(flavourTextLine);
+	flavourTextLine.innerHTML=currentItem.flavourtext;
+
+	var basicStats = document.createElement('p');
+	document.getElementById('currentItemDiv').appendChild(basicStats);
+	var basicStatsString = "Category: "+currentItem.category
+	var node = document.createTextNode(basicStatsString);
+	basicStats.appendChild(node);
+	basicStats.innerHTML+="<br>";
+	if (currentItem.cookinglevel!==undefined){
+		basicStats.innerHTML+="Cooking Lv: "+currentItem.cookinglevel
+    }
+	if (currentItem.forginglevel!==undefined){
+		basicStats.innerHTML+="Forging Lv: "+currentItem.forginglevel
+    }
+	if (currentItem.craftinglevel!==undefined){
+		basicStats.innerHTML+="Crafting Lv: "+currentItem.craftinglevel
+    }
+	if (currentItem.chemistrylevel!==undefined){
+		basicStats.innerHTML+="Chemistry Lv: "+currentItem.chemistrylevel
+    }
+
+	if (currentItem.buy!==undefined){
+		currentBuyPrice=currentItem.buy;
+		currentBuyPriceDiscount=Math.ceil(currentBuyPrice*.9);
+	}
+
+	var priceLine = document.createElement('p');
+	document.getElementById('currentItemDiv').appendChild(priceLine);
+	var priceString="Base Sell Price: "+currentSellPrice+" G";
+	var node = document.createTextNode(priceString);
+	priceLine.appendChild(node);
+	if (currentItem.buy!==undefined){
+		priceLine.innerHTML+="<br>Base Buy Price: "+currentBuyPrice+" G";
+		priceLine.innerHTML+=" (with discount: "+currentBuyPriceDiscount+" G)";
+	}
+}
+    
+var materialsList
+
+var currentMaterialsBuyPrice
+var currentMaterialsBuyPriceDiscount
