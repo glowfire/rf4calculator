385bf5a6bd28e4fb818ce43133acb4dbb08ab416
glowfire <kunchyi1@gmail.com>
2016-07-16 23:35:41 +0800

    Create categories.js

---
+++ items/other/categories.js
@@ -0,0 +1,8 @@
+// For the categories as listed in recipes' material lists.
+
+registerItem({
+	"name":"Squid (Category)",
+	"category":"Material",
+	"flavourtext":"A mollusk that's active at night. Uses the fins on its body to swim like it's flying in the water."
+	"members":["Squid","Sunsquid"]
+})
