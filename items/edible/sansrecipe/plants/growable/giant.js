799b7586abcb001b689253411464d56347b38388
glowfire <kunchyi1@gmail.com>
2016-07-16 19:56:59 +0800

    Delete giant.js

--- items/edible/sansrecipe/plants/growable/giant.js
+++
@@ -1,3 +0,0 @@
-// Complete
-// Toyherb, Pink Cat, Moondrop, Charm Blue, Cherry Grass, Pom-Pom Grass, Lamp Grass, Ironleaf, Clover, Autumn Grass, Noel Grass,
-// Fireflower, Emery Flower, Blue, Green, Red, White Crystal
