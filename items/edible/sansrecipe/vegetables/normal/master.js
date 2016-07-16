a54e8cb873787813aa9686b543120afe55ad11e7
glowfire <kunchyi1@gmail.com>
2016-07-16 23:23:28 +0800

    Delete master.js

--- items/edible/sansrecipe/vegetables/normal/master.js
+++
@@ -1,3 +0,0 @@
-// Complete
-// Turnip, Potato, Spinach, Radish, Yam, Pumpkin, Cucumber, Carrot, Corn, Strawberry, Leek, Pink Turnip, Green Pepper, Eggplant, Hot-hot,
-// Bok Choy, Cabbage, Onion, Tomato, Pink Melon, Pineapple, G Potato, G Pumpkin, G Cabbage, G Turnip, in that orde
