9eb5e9a6a687216234d700caab25b893c4dfaac9
glowfire <kunchyi1@gmail.com>
2016-07-16 18:49:06 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -829,3 +829,25 @@ registerItem({
 		"hp":5
 	}
 })
+
+registerItem({
+	"name":"Pineapple Seeds",
+	"sell":280,
+	"buy":2800,
+	"value":1600,
+	"growth":28,
+	"harvested":3,
+	"flavourtext":"Grows well in summer, but not in other seasons. Takes time to grow. May grow again after harvesting, but tough on soil.",
+	"regrow":false,
+	"seasons":{
+		"spring":"bad",
+		"summer":"good",
+		"autumn":"bad",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
