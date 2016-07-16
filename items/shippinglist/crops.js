8fc57bad06939a0dfbdef6ad0a9117a5e7ae38df
glowfire <kunchyi1@gmail.com>
2016-07-16 19:54:55 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -1969,3 +1969,198 @@ registerItem({
 		"hp":20
 	}
 })
+
+registerItem({
+	"name":"Great Charm Blue",
+	"category":"Plant",
+	"sell":1200,
+	"pricemultiplier":"E",
+	"flavourtext":"A deep blue flower. Practically swallows you into its beautiful blossoms.",
+	"effects":{
+		"statusailments":["PSN"],
+		"rp":80
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":46,
+		"int":8,
+		"psnatkP":17
+	},
+	"cookingeffects":{
+		"statusailments":["PSN"],
+		"rp":80,
+		"hpmax":100,
+		"str":5,
+		"vit":5,
+		"int":5,
+		"slpresP":75,
+		"ftgresP":75,
+		"sickresP":75
+	}
+})
+
+registerItem({
+	"name":"Charm Blue",
+	"category":"Plant",
+	"sell":220,
+	"buy":800,
+	"pricemultiplier":"E",
+	"flavourtext":"A deep blue flower. Practically swallows you into its beautiful blossoms.",
+	"effects":{
+		"statusailments":["PSN"],
+		"rp":28
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":20,
+		"psnatkP":5
+	},
+	"cookingeffects":{
+		"statusailments":["PSN"],
+		"rp":28,
+		"hpmax":10,
+		"slpresP":10,
+		"ftgresP":10,
+		"sickresP":10
+	}
+})
+
+registerItem({
+	"name":"King Pink Cat",
+	"category":"Plant",
+	"sell":600,
+	"pricemultiplier":"E",
+	"flavourtext":"Flower with vibrant pink blossoms. Named 'cat' for its cuteness.",
+	"effects":{
+		"rp":65
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":38,
+		"int":5
+	},
+	"cookingeffects":{
+		"rp":65,
+		"psnresP":25,
+		"sealresP":25,
+		"parresP":25,
+		"slpresP":25,
+		"ftgresP":25,
+		"sickresP":25
+	}
+})
+
+registerItem({
+	"name":"Pink Cat",
+	"category":"Plant",
+	"sell":110,
+	"buy":450,
+	"pricemultiplier":"E",
+	"flavourtext":"Flower with vibrant pink blossoms. Named 'cat' for its cuteness.",
+	"effects":{
+		"rp":25
+	},
+	"upgradeinfo":{
+		"rarity":2,
+		"difficulty":15,
+		"int":2
+	},
+	"cookingeffects":{
+		"rp":25,
+		"psnresP":5,
+		"sealresP":5,
+		"parresP":5,
+		"slpresP":5,
+		"ftgresP":5,
+		"sickresP":5
+	}
+})
+
+registerItem({
+	"name":"Ultra Moondrop Flower",
+	"category":"Plant",
+	"sell":650,
+	"pricemultiplier":"E",
+	"flavourtext":"A unique and beautiful yellow flower that grows in the moonlight.",
+	"effects":{
+		"rp":58
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":35,
+		"int":4
+	},
+	"cookingeffects":{
+		"rp":58,
+		"hpmax":50,
+		"str":3,
+		"vit":3,
+		"int":3
+	}
+})
+
+registerItem({
+	"name":"Moondrop Flower",
+	"category":"Plant",
+	"sell":115,
+	"buy":400,
+	"pricemultiplier":"E",
+	"flavourtext":"A unique and beautiful yellow flower that grows in the moonlight.",
+	"effects":{
+		"rp":20
+	},
+	"upgradeinfo":{
+		"rarity":1,
+		"difficulty":10,
+		"int":1
+	},
+	"cookingeffects":{
+		"rp":20
+	}
+})
+
+registerItem({
+	"name":"Ultra Toyherb",
+	"category":"Plant",
+	"sell":350,
+	"pricemultiplier":"E",
+	"flavourtext":"An herb that promotes good health. Hard to grow, but cures fatigue.",
+	"effects":{
+		"heal":["FTG"],
+		"rp":37
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":26,
+		"ftgresP":30
+	},
+	"cookingeffects":{
+		"heal":["FTG"],
+		"rp":37,
+		"ftgresP":100
+	}
+})
+
+registerItem({
+	"name":"Toyherb",
+	"category":"Plant",
+	"sell":65,
+	"buy":250,
+	"pricemultiplier":"E",
+	"flavourtext":"An herb that promotes good health. Hard to grow, but cures fatigue.",
+	"effects":{
+		"heal":["FTG"],
+		"rp":15
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1,
+		"vit":1,
+		"ftgresP":5
+	},
+	"cookingeffects":{
+		"heal":["FTG"],
+		"rp":15,
+		"ftgresP":15
+	}
+})
