9cc5ee1b4554193b5330f3f2554ffdcf2fd21d03
glowfire <kunchyi1@gmail.com>
2016-07-16 18:19:23 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -1,1 +1,149 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the first slot.
 
+registerItem({
+	"name":"Shield Seed",
+	"sell":150,
+	"buy":1500,
+	"value":1e3,
+	"growth":12,
+	"harvested":1,
+	"flavourtext":"Grows into a shield. Cool, right?",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Sword Seed",
+	"sell":150,
+	"buy":1500,
+	"value":1e3,
+	"growth":12,
+	"harvested":1,
+	"flavourtext":"Grows into a sword. Neat, huh?",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Dungeon Seed",
+	"sell":500,
+	"buy":5000,
+	"growth":18,
+	"flavourtext":"Grows to a dungeon. Depending on the level of the seed, it may bloom into a particularly difficult one.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Shining Seed",
+	"sell":300,
+	"buy":3000,
+	"growth":38,
+	"flavourtext":"Doesn't grow well in winter. Produces a lot of lumber. May produce ore.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Grape Tree Seed",
+	"sell":600,
+	"buy":6000,
+	"value":120,
+	"growth":50,
+	"flavourtext":"Doesn't grow well in winter. Once it grows into a tree, it will produce grapes every day.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Orange Tree Seed",
+	"sell":500,
+	"buy":5000,
+	"value":80,
+	"growth":22,
+	"flavourtext":"Doesn't grow well in winter. Once it grows into a tree, it will produce oranges every day.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Apple Tree Seed",
+	"sell":700,
+	"buy":7000,
+	"value":150,
+	"growth":62,
+	"flavourtext":"Doesn't grow well in winter. Once it grows into a tree, it will produce apples every day.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
