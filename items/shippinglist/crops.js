3d7a6ac5007e3b96f5d7afbaabe9aa9a4e261701
glowfire <kunchyi1@gmail.com>
2016-07-16 18:58:24 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -1005,3 +1005,25 @@ registerItem({
 		"hp":5
 	}
 })
+
+registerItem({
+	"name":"Strawberry Seeds",
+	"sell":40,
+	"buy":400,
+	"value":135,
+	"growth":16,
+	"harvested":6,
+	"flavourtext":"Grows well in spring and fall. Bears sweet fruits. Hearty ones may grow again after harvesting.",
+	"regrow":true,
+	"seasons":{
+		"spring":"good",
+		"summer":"normal",
+		"autumn":"good",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
