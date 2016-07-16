5ebafc5065f0b2a6820ab99bd4765725e5eca4d0
glowfire <kunchyi1@gmail.com>
2016-07-16 21:09:01 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -2633,3 +2633,796 @@ registerItem({
 	"flavourtext":"Known for its crunchy texture and fragrant allure. Grows quickly during spring.",
 	"cookingeffects":{}
 })
+
+registerItem({
+	"name":"Giant Hot-Hot Fruit",
+	"category":"Vegetable",
+	"sell":4500,
+	"pricemultiplier":"E",
+	"flavourtext":"Covered in fuzz. Some use it to boil water.",
+	"effects":{
+		"hp":300,
+		"rp":140
+	},
+	"upgradeinfo":{
+		"rarity":9,
+		"difficulty":55,
+		"mdef":11,
+		"fireresP":15
+	},
+	"cookingeffects":{
+		"hp":300,
+		"rp":140,
+		"fireresP":15,
+		"slpresP":50
+	}
+})
+
+registerItem({
+	"name":"Hot-Hot Fruit",
+	"category":"Vegetable",
+	"sell":900,
+	"buy":7000,
+	"pricemultiplier":"E",
+	"flavourtext":"Covered in fuzz. Some use it to boil water.",
+	"effects":{
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":35,
+		"mdef":7,
+		"fireresP":5
+	},
+	"cookingeffects":{
+		"fireresP":5,
+		"slpresP":10
+	}
+})
+
+registerItem({
+	"name":"Boss Bok Choy",
+	"category":"Vegetable",
+	"sell":2250,
+	"pricemultiplier":"E",
+	"flavourtext":"A popular winter crop. The colder it is, the sweeter it gets.",
+	"effects":{
+		"hp":550
+	},
+	"upgradeinfo":{
+		"rarity":10,
+		"difficulty":60,
+		"matk":21
+	},
+	"cookingeffects":{
+		"hp":550,
+		"sealresP":50
+	}
+})
+
+registerItem({
+	"name":"Bok Choy",
+	"category":"Vegetable",
+	"sell":420,
+	"buy":1900,
+	"pricemultiplier":"E",
+	"flavourtext":"A popular winter crop. The colder it is, the sweeter it gets.",
+	"effects":{
+		"hp":130
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":36,
+		"matk":8
+	},
+	"cookingeffects":{
+		"hp":130,
+		"sealresP":10
+	}
+})
+
+registerItem({
+	"name":"Legendary Leek",
+	"category":"Vegetable",
+	"sell":8000,
+	"pricemultiplier":"E",
+	"flavourtext":"A must-have vegetable for winter dishes such as hot pot.",
+	"effects":{
+		"hp":280
+	},
+	"upgradeinfo":{
+		"rarity":9,
+		"difficulty":54,
+		"matk":11
+	},
+	"cookingeffects":{
+		"hp":280,
+		"sickresP":50
+	}
+})
+
+registerItem({
+	"name":"Leek",
+	"category":"Vegetable",
+	"sell":1600,
+	"buy":4800,
+	"pricemultiplier":"E",
+	"flavourtext":"A must-have vegetable for winter dishes such as hot pot.",
+	"effects":{
+		"hp":120
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":35,
+		"matk":6
+	},
+	"cookingeffects":{
+		"hp":120,
+		"sickresP":10
+	}
+})
+
+registerItem({
+	"name":"Noble Radish",
+	"category":"Vegetable",
+	"sell":2000,
+	"pricemultiplier":"E",
+	"flavourtext":"A white, thick <q>daikon</q> radish. Used in many home-cooking recipes.",
+	"effects":{
+		"hp":168
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":38,
+		"vit":12
+	},
+	"cookingeffects":{
+		"hp":168,
+		"critresP":20
+	}
+})
+
+registerItem({
+	"name":"Radish",
+	"category":"Vegetable",
+	"sell":360,
+	"buy":1580,
+	"pricemultiplier":"E",
+	"flavourtext":"A white, thick <q>daikon</q> radish. Used in many home-cooking recipes.",
+	"effects":{
+		"hp":5
+	},
+	"upgradeinfo":{
+		"rarity":2,
+		"difficulty":12,
+		"vit":3
+	},
+	"cookingeffects":{
+		"hp":5,
+		"critresP":5
+	}
+})
+
+registerItem({
+	"name":"Tyrant Turnip",
+	"category":"Vegetable",
+	"sell":200,
+	"pricemultiplier":"E",
+	"flavourtext":"The king of all crops. It has been through many hardships to stay in this position. Bow down to the turnip.",
+	"effects":{
+		"hp":72
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":25,
+		"str":5,
+		"loveresP":15
+	},
+	"cookingeffects":{
+		"hp":72,
+		"str":5,
+		"loveresP":15
+	}
+})
+
+registerItem({
+	"name":"Turnip",
+	"category":"Vegetable",
+	"sell":30,
+	"buy":200,
+	"pricemultiplier":"E",
+	"flavourtext":"The king of all crops. It has been through many hardships to stay in this position. Bow down to the turnip.",
+	"effects":{
+		"hp":30,
+		"rp":5
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1,
+		"str":1
+	},
+	"cookingeffects":{
+		"hp":30,
+		"rp":5,
+		"str":1
+	}
+})
+
+registerItem({
+	"name":"Sovereign Spinach",
+	"category":"Vegetable",
+	"sell":350,
+	"pricemultiplier":"E",
+	"flavourtext":"A dark green vegetable rich in nutrients. Said to give you strength and energy.",
+	"effects":{
+		"hp":115
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":35,
+		"matk":7
+	},
+	"cookingeffects":{
+		"hp":115,
+		"int":5
+	}
+})
+
+registerItem({
+	"name":"Spinach",
+	"category":"Vegetable",
+	"sell":65,
+	"buy":300,
+	"pricemultiplier":"E",
+	"flavourtext":"A dark green vegetable rich in nutrients. Said to give you strength and energy.",
+	"effects":{
+		"hp":50
+	},
+	"upgradeinfo":{
+		"rarity":1,
+		"difficulty":9,
+		"matk":2
+	},
+	"cookingeffects":{
+		"hp":50,
+		"int":1
+	}
+})
+
+registerItem({
+	"name":"Green Pepper Rex",
+	"category":"Vegetable",
+	"sell":800,
+	"pricemultiplier":"E",
+	"flavourtext":"Although nutritious and tasty when prepared right, green peppers are sometimes disliked for their bitterness.",
+	"effects":{
+		"hp":250
+	},
+	"upgradeinfo":{
+		"rarity":9,
+		"difficulty":53,
+		"str":16
+	},
+	"cookingeffects":{
+		"hp":250,
+		"vit":30
+	}
+})
+
+registerItem({
+	"name":"Green Pepper",
+	"category":"Vegetable",
+	"sell":150,
+	"buy":700,
+	"pricemultiplier":"E",
+	"flavourtext":"Although nutritious and tasty when prepared right, green peppers are sometimes disliked for their bitterness.",
+	"effects":{
+		"hp":90
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":31,
+		"str":6
+	},
+	"cookingeffects":{
+		"hp":90,
+		"vit":8
+	}
+})
+
+registerItem({
+	"name":"Royal Carrot",
+	"category":"Vegetable",
+	"sell":2000,
+	"pricemultiplier":"E",
+	"flavourtext":"A long and thin vegetable with an orange color. Horses love to eat them, snowmen love to wear them, and children love to hate them.",
+	"effects":{
+		"hp":240
+	},
+	"upgradeinfo":{
+		"rarity":8,
+		"difficulty":51,
+		"vit":21
+	},
+	"cookingeffects":{
+		"hp":240,
+		"critresP":50
+	}
+})
+
+registerItem({
+	"name":"Carrot",
+	"category":"Vegetable",
+	"sell":380,
+	"buy":2600,
+	"pricemultiplier":"E",
+	"flavourtext":"A long and thin vegetable with an orange color. Horses love to eat them, snowmen love to wear them, and children love to hate them.",
+	"effects":{
+		"hp":80
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":27,
+		"vit":7
+	},
+	"cookingeffects":{
+		"hp":80,
+		"critresP":8
+	}
+})
+
+registerItem({
+	"name":"Princely Potato",
+	"category":"Vegetable",
+	"sell":500,
+	"pricemultiplier":"E",
+	"flavourtext":"Loved by people from around the world. Used in various recipes from appetizers to main dishes.",
+	"effects":{
+		"hp":110
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":34,
+		"vit":9
+	},
+	"cookingeffects":{
+		"hp":110,
+		"hpmax":100
+	}
+})
+
+registerItem({
+	"name":"Potato",
+	"category":"Vegetable",
+	"sell":90,
+	"buy":600,
+	"pricemultiplier":"E",
+	"flavourtext":"Loved by people from around the world. Used in various recipes from appetizers to main dishes.",
+	"effects":{
+		"hp":40
+	},
+	"upgradeinfo":{
+		"rarity":1,
+		"difficulty":7,
+		"vit":2
+	},
+	"cookingeffects":{
+		"hp":40,
+		"hpmax":15
+	}
+})
+
+registerItem({
+	"name":"Lordly Yam",
+	"category":"Vegetable",
+	"sell":800,
+	"pricemultiplier":"E",
+	"flavourtext":"A vegetable high in fiber that will scour your gut and pipes.",
+	"effects":{
+		"hp":200
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":44,
+		"vit":15
+	},
+	"cookingeffects":{
+		"hp":200,
+		"hpmax":200
+	}
+})
+
+registerItem({
+	"name":"Yam",
+	"category":"Vegetable",
+	"sell":140,
+	"buy":450,
+	"pricemultiplier":"E",
+	"flavourtext":"A vegetable high in fiber that will scour your gut and pipes.",
+	"effects":{
+		"hp":60
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":18,
+		"vit":5
+	},
+	"cookingeffects":{
+		"hp":60,
+		"hpmax":50
+	}
+})
+
+registerItem({
+	"name":"Emperor Eggplant",
+	"category":"Vegetable",
+	"sell":1000,
+	"pricemultiplier":"E",
+	"flavourtext":"A purple vegetable with a cute stem. Has a soft texture.",
+	"effects":{
+		"hp":270
+	},
+	"upgradeinfo":{
+		"rarity":9,
+		"difficulty":54,
+		"str":17
+	},
+	"cookingeffects":{
+		"hp":270,
+		"critP":10
+	}
+})
+
+registerItem({
+	"name":"Eggplant",
+	"category":"Vegetable",
+	"sell":190,
+	"buy":950,
+	"pricemultiplier":"E",
+	"flavourtext":"A purple vegetable with a cute stem. Has a soft texture.",
+	"effects":{
+		"hp":100
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":33,
+		"str":7
+	},
+	"cookingeffects":{
+		"hp":100,
+		"critP":1
+	}
+})
+
+registerItem({
+	"name":"Titan Tomato",
+	"category":"Vegetable",
+	"sell":3000,
+	"pricemultiplier":"E",
+	"flavourtext":"A brilliant tomato, ruddy red like the summer sun. Popular for its high nutrition and health benefits.",
+	"effects":{
+		"hp":700
+	},
+	"upgradeinfo":{
+		"rarity":11,
+		"difficulty":66,
+		"str":28
+	},
+	"cookingeffects":{
+		"hp":700,
+		"psnresP":50
+	}
+})
+
+registerItem({
+	"name":"Tomato",
+	"category":"Vegetable",
+	"sell":540,
+	"buy":1800,
+	"pricemultiplier":"E",
+	"flavourtext":"A brilliant tomato, ruddy red like the summer sun. Popular for its high nutrition and health benefits.",
+	"effects":{
+		"hp":180
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":43,
+		"str":9
+	},
+	"cookingeffects":{
+		"hp":180,
+		"psnresP":10
+	}
+})
+
+registerItem({
+	"name":"Gigant Corn",
+	"category":"Vegetable",
+	"sell":7500,
+	"pricemultiplier":"E",
+	"flavourtext":"A vegetable studded with yellow kernels. Hard to decide which side to eat from. Best lathered with butter.",
+	"effects":{
+		"hp":230
+	},
+	"upgradeinfo":{
+		"rarity":8,
+		"difficulty":49,
+		"vit":17
+	},
+	"cookingeffects":{
+		"hp":230,
+		"rpmax":50
+	}
+})
+
+registerItem({
+	"name":"Corn",
+	"category":"Vegetable",
+	"sell":1e3,
+	"buy":3e3,
+	"pricemultiplier":"E",
+	"flavourtext":"A vegetable studded with yellow kernels. Hard to decide which side to eat from. Best lathered with butter.",
+	"effects":{
+		"hp":75
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":27,
+		"vit":6
+	},
+	"cookingeffects":{
+		"hp":75,
+		"rpmax":10
+	}
+})
+
+registerItem({
+	"name":"Kaiser Cucumber",
+	"category":"Vegetable",
+	"sell":350,
+	"pricemultiplier":"E",
+	"flavourtext":"A green and watery vegetable with a crunchy texture. Have a bite and feel refreshed!",
+	"effects":{
+		"hp":175
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":42,
+		"str":8
+	},
+	"cookingeffects":{
+		"hp":175,
+		"vit":15
+	}
+})
+
+registerItem({
+	"name":"Cucumber",
+	"category":"Vegetable",
+	"sell":68,
+	"buy":250,
+	"pricemultiplier":"E",
+	"flavourtext":"A green and watery vegetable with a crunchy texture. Have a bite and feel refreshed!",
+	"effects":{
+		"hp":55
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":17,
+		"str":2
+	},
+	"cookingeffects":{
+		"hp":55,
+		"vit":2
+	}
+})
+
+registerItem({
+	"name":"Doom Pumpkin",
+	"category":"Vegetable",
+	"sell":550,
+	"pricemultiplier":"E",
+	"flavourtext":"A nutritious seasonal vegetable. Can be worn during the autumn festival by scooping out the inside.",
+	"effects":{
+		"hp":210
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":45,
+		"str":10
+	},
+	"cookingeffects":{
+		"hp":210,
+		"vit":20
+	}
+})
+
+registerItem({
+	"name":"Pumpkin",
+	"category":"Vegetable",
+	"sell":110,
+	"buy":420,
+	"pricemultiplier":"E",
+	"flavourtext":"A nutritious seasonal vegetable. Can be worn during the autumn festival by scooping out the inside.",
+	"effects":{
+		"hp":65
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":19,
+		"str":3
+	},
+	"cookingeffects":{
+		"hp":65,
+		"vit":4
+	}
+})
+
+registerItem({
+	"name":"Ultra Onion",
+	"category":"Vegetable",
+	"sell":4000,
+	"pricemultiplier":"E",
+	"flavourtext":"When raw it has a sharp taste, but turns mild when cooked. Many cry chopping onions, but those tears aren't for you!",
+	"effects":{
+		"hp":650
+	},
+	"upgradeinfo":{
+		"rarity":10,
+		"difficulty":62,
+		"vit":35
+	},
+	"cookingeffects":{
+		"hp":650,
+		"parresP":50
+	}
+})
+
+registerItem({
+	"name":"Onion",
+	"category":"Vegetable",
+	"sell":800,
+	"buy":6700,
+	"pricemultiplier":"E",
+	"flavourtext":"When raw it has a sharp taste, but turns mild when cooked. Many cry chopping onions, but those tears aren't for you!",
+	"effects":{
+		"hp":150
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":37,
+		"vit":10
+	},
+	"cookingeffects":{
+		"hp":150,
+		"parresP":10
+	}
+})
+
+registerItem({
+	"name":"Conqueror Melon",
+	"category":"Vegetable",
+	"sell":4250,
+	"pricemultiplier":"E",
+	"flavourtext":"A pink melon overflowing with cuteness. Attracts young girls with its adorable shape and taste. Truly heinous.",
+	"effects":{
+		"hp":300,
+		"rp":150
+	},
+	"upgradeinfo":{
+		"rarity":11,
+		"difficulty":69,
+		"int":31
+	},
+	"cookingeffects":{
+		"hp":300,
+		"rp":150,
+		"str":10,
+		"int":10
+	}
+})
+
+registerItem({
+	"name":"Pink Melon",
+	"category":"Vegetable",
+	"sell":850,
+	"buy":5000,
+	"pricemultiplier":"E",
+	"flavourtext":"A pink melon overflowing with cuteness. Attracts young girls with its adorable shape and taste. Truly heinous.",
+	"effects":{
+		"hp":90,
+		"rp":30
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":45,
+		"int":7
+	},
+	"cookingeffects":{
+		"hp":90,
+		"rp":30,
+		"str":5,
+		"int":5
+	}
+})
+
+registerItem({
+	"name":"Colossal Pink",
+	"category":"Vegetable",
+	"sell":1000,
+	"pricemultiplier":"E",
+	"flavourtext":"A pretty pink-colored turnip. Raised through the efforts of a turnip-obsessed girl. Famous for how easy it grows.",
+	"effects":{
+		"hp":220
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":47,
+		"str":12
+	},
+	"cookingeffects":{
+		"hp":220,
+		"str":15
+	}
+})
+
+registerItem({
+	"name":"Pink Turnip",
+	"category":"Vegetable",
+	"sell":200,
+	"buy":700,
+	"pricemultiplier":"E",
+	"flavourtext":"A pretty pink-colored turnip. Raised through the efforts of a turnip-obsessed girl. Famous for how easy it grows.",
+	"effects":{
+		"hp":70
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":21,
+		"str":4
+	},
+	"cookingeffects":{
+		"hp":70,
+		"str":2
+	}
+})
+
+registerItem({
+	"name":"King Cabbage",
+	"category":"Vegetable",
+	"sell":3400,
+	"pricemultiplier":"E",
+	"flavourtext":"A well-balanced vegetable that can be eaten raw, fried or boiled. Helps with digestion after a big meal.",
+	"effects":{
+		"hp":600
+	},
+	"upgradeinfo":{
+		"rarity":10,
+		"difficulty":61,
+		"int":22
+	},
+	"cookingeffects":{
+		"hp":600,
+		"int":15
+	}
+})
+
+registerItem({
+	"name":"Cabbage",
+	"category":"Vegetable",
+	"sell":680,
+	"buy":2800,
+	"pricemultiplier":"E",
+	"flavourtext":"A well-balanced vegetable that can be eaten raw, fried or boiled. Helps with digestion after a big meal.",
+	"effects":{
+		"hp":140
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":37,
+		"int":6
+	},
+	"cookingeffects":{
+		"hp":140,
+		"int":2
+	}
+})
