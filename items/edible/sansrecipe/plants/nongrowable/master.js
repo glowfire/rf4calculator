36244dee87c54b4340b7a2f33454a836b258bcfb
glowfire <kunchyi1@gmail.com>
2016-07-16 18:01:28 +0800

    Update master.js

--- items/edible/sansrecipe/plants/nongrowable/master.js
+++ items/edible/sansrecipe/plants/nongrowable/master.js
@@ -1,19 +1,288 @@
+
+
 registerItem({
-  "name":"Withered Grass",
-  "category":"Plant",
-  "sell":1,
-  "buy":100,
-  "pricemultiplier":"E",
-  "effects":{
-    "statusailments":["PSN"],
-    "rpP":-20
-  },
-  "upgradeinfo":{
-    "rarity":1,//unknown
-    "difficulty":1,
-    "vit":-1
-  },
-  "ingredienteffects":{
-    "rp":20//unknown
-  }
+	"name":"Elli Leaves",
+	"category":"Plant",
+	"sell":250,
+	"buy":3500,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":400,
+		"rp":5
+	},
+	"upgradeinfo":{
+		"rarity":10,
+		"difficulty":60
+	},
+	"flavourtext":"An exotic plant with a dark, blue flower. Beautiful!",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Ayngondaia Lawn",
+	"category":"Plant",
+	"sell":33,
+	"pricemultiplier":"E", ///////////////////////////////////// unknown
+	"effects":{
+	},
+	"upgradeinfo":{
+		"rarity":10, ///////////////////////////////////// unknown
+		"difficulty":30
+	},
+	"flavourtext":"A rare plant. Imbued with incredible vitality, rumor has it that this bright bounty has rejuvenating qualities.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Withered Grass",
+	"category":"Plant",
+	"sell":1,
+	"buy":100,
+	"pricemultiplier":"E",
+	"effects":{
+		"statusailments":["PSN"],
+		"rpP":-20
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1,
+		"vit":-1
+    },
+    "flavourtext":"You'd get sick if you ate this grass, but it can be used to improve the quality of soil.",
+	"ingredienteffects":{
+    	"rp":20//unknown
+    }
+})
+
+registerItem({
+	"name":"Weeds",
+	"category":"Plant",
+	"sell":1,
+	"buy":30,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":-5
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1
+	},
+	"flavourtext":"Grows anywhere, anytime. It doesn't obey any rules. It does its own thing. And that's why it's cool.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"White Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":200,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":8,
+		"difficulty":65
+	},
+	"flavourtext":"Beautiful grass of purest white. Hard to find in winter since it blends in with the snow. Fragile, but noble.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Indigo Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":80,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":4,
+		"difficulty":29
+	},
+	"flavourtext":"Soothing grass that supposedly resolves arguments just by being there.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Purple Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":20,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":1,
+		"difficulty":5
+	},
+	"flavourtext":"A sophisticated purple grass. Has trouble getting along with the other grasses due to its massive ego.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Green Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1
+	},
+	"flavourtext":"Plain green grass. Trying hard to change so people will stop calling it boring.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Blue Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":30,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":11
+	},
+	"flavourtext":"A nifty blue grass that's cold to the touch. Used in medicine due to its soothing effect.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Yellow Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":100,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":5,
+		"difficulty":39
+	},
+	"flavourtext":"Grass which brings you luck. People carry it in hopes that it will make them rich.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Red Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":120,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":6,
+		"difficulty":40
+	},
+	"flavourtext":"Grass of passion. Its red color helps it grow faster than other grasses.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Orange Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":150,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":7,
+		"difficulty":48
+	},
+	"flavourtext":"Grass as orange as the sun. Its bright color gives you energy just by looking at it.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Black Grass",
+	"category":"Plant",
+	"sell":5,
+	"buy":120,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":50,
+		"rp":3
+	},
+	"upgradeinfo":{
+		"rarity":3,
+		"difficulty":23
+	},
+	"flavourtext":"Grass with a dull, mysterious glow like a muffled black light. Hard to see at night, so it's often stepped on.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Antidote Grass",
+	"category":"Plant",
+	"sell":6,
+	"buy":150,
+	"pricemultiplier":"E",
+	"effects":{
+		"heal":["PSN"],
+		"hp":5
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":8,
+		"psnresP":3
+	},
+	"flavourtext":"A grass that purifies poisons in the body. Very bitter, but effective.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Medicinal Herb",
+	"category":"Plant",
+	"sell":8,
+	"buy":80,
+	"pricemultiplier":"E",
+	"effects":{
+		"hp":20
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":2,
+		"ftgresP":1
+	},
+	"flavourtext":"A wild herb with a bitter taste. Helps regain energy.",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Bamboo Sprout",
+	"category":"Vegetable",
+	"sell":40,
+	"buy":200,
+	"pricemultiplier":"E",
+	"effects":{
+		"rp":8
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1,
+		"atk":1
+	},
+	"flavourtext":"Known for its crunchy texture and fragrant allure. Grows quickly during spring.",
+	"cookingeffects":{}
 })
