4e7df0c90032aa6abe9e354d6528d46588161aa4
glowfire <kunchyi1@gmail.com>
2016-07-16 20:58:09 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -2615,3 +2615,21 @@ registerItem({
 		"intP":3
 	}
 })
+
+registerItem({
+	"name":"Bamboo Sprout",
+	"category":"Vegetable",
+	"sell":40,
+	"buy":200,
+	"pricemultiplier":"E",
+	"effects":{
+		"rp":8
+	},
+	"upgradeinfo":{
+		"rarity":0,
+		"difficulty":1,
+		"atk":1
+	},
+	"flavourtext":"Known for its crunchy texture and fragrant allure. Grows quickly during spring.",
+	"cookingeffects":{}
+})
