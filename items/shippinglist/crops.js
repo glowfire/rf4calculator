0fdf38c651542f53f2cef4e922b75912d110931f
glowfire <kunchyi1@gmail.com>
2016-07-16 18:29:41 +0800

    Update crops.js

--- items/shippinglist/crops.js
+++ items/shippinglist/crops.js
@@ -367,3 +367,289 @@ registerItem({
 		"hp":5
 	}
 })
+
+registerItem({
+	"name":"White Crystal Seeds",
+	"sell":1e3,
+	"buy":3e4,
+	"value":22500,
+	"growth":90,
+	"harvested":3,
+	"flavourtext":"Grows well in winter, but not in other seasons. Takes a long time to grow. Its seed resembles white crystal.",
+	"regrow":false,
+	"seasons":{
+		"spring":"bad",
+		"summer":"bad",
+		"autumn":"bad",
+		"winter":"good"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Red Crystal Seeds",
+	"sell":1e3,
+	"buy":3e4,
+	"value":2e4,
+	"growth":80,
+	"harvested":3,
+	"flavourtext":"Grows well in fall, but not in other seasons. Takes a long time to grow. Its seed resembles red crystal.",
+	"regrow":false,
+	"seasons":{
+		"spring":"bad",
+		"summer":"bad",
+		"autumn":"good",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Pom-Pom Grass Sds.",
+	"sell":51,
+	"buy":510,
+	"value":440,
+	"growth":14,
+	"harvested":3,
+	"flavourtext":"Grows well in fall, but not in winter. Takes a long time to grow. Its seed is white and soft.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"good",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Autumn Grass Seeds",
+	"sell":250,
+	"buy":2500,
+	"value":1800,
+	"growth":29,
+	"harvested":3,
+	"flavourtext":"Grows well in fall, but not in winter. Takes a long time to grow. Its seed is red and round.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"good",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Noel Grass Seeds",
+	"sell":270,
+	"buy":2700,
+	"value":2e3,
+	"growth":33,
+	"harvested":4,
+	"flavourtext":"Grows well in winter, but not in summer. Takes a long time to grow but rarely withers. Used in certain festivals.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"bad",
+		"autumn":"normal",
+		"winter":"good"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Green Crystal Seeds",
+	"sell":1e3,
+	"buy":3e4,
+	"value":17e3,
+	"growth":70,
+	"harvested":3,
+	"flavourtext":"Grows well in summer, but not in other seasons. Takes a long time to grow. Its seed resembles green crystal.",
+	"regrow":false,
+	"seasons":{
+		"spring":"bad",
+		"summer":"good",
+		"autumn":"bad",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Fireflower Seeds",
+	"sell":500,
+	"buy":5000,
+	"value":2800,
+	"growth":42,
+	"harvested":4,
+	"flavourtext":"Grows well in summer, but not in winter. Its seeds are as black as gunpowder.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"good",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Clover Seeds",
+	"sell":96,
+	"buy":960,
+	"value":980,
+	"growth":28,
+	"harvested":3,
+	"flavourtext":"Grows well in spring and fall, but not in summer. A lucky seed that brings good fortune, but withers very easily.",
+	"regrow":false,
+	"seasons":{
+		"spring":"good",
+		"summer":"bad",
+		"autumn":"good",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Ironleaf Seeds",
+	"sell":60,
+	"buy":600,
+	"value":1111,
+	"growth":21,
+	"harvested":2,
+	"flavourtext":"Grows well in winter, but not in summer. Takes a long time to grow. Has a special power to gather iron from the soil.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"bad",
+		"autumn":"normal",
+		"winter":"good"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Emery Flower Seeds",
+	"sell":2500,
+	"buy":5e4,
+	"value":55e3,
+	"growth":120,
+	"harvested":2,
+	"flavourtext":"Takes a very long time to grow. Rarely withers, and can be cultivated by almost anyone.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"normal"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Blue Crystal Seeds",
+	"sell":1e3,
+	"buy":3e4,
+	"value":15e3,
+	"growth":55,
+	"harvested":3,
+	"flavourtext":"Grows well in spring, but not in other seasons. Takes a long time to grow. Its seed resembles blue crystal.",
+	"regrow":false,
+	"seasons":{
+		"spring":"good",
+		"summer":"bad",
+		"autumn":"bad",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Lamp Grass Seeds",
+	"sell":74,
+	"buy":740,
+	"value":650,
+	"growth":16,
+	"harvested":3,
+	"flavourtext":"Grows well in winter. Takes a long time to grow. Mainly used as an ingredient in medicines.",
+	"regrow":false,
+	"seasons":{
+		"spring":"normal",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"good"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
+
+registerItem({
+	"name":"Cherry Grass Seeds",
+	"sell":42,
+	"buy":420,
+	"value":230,
+	"growth":10,
+	"harvested":4,
+	"flavourtext":"Grows well in spring, but not in winter. Grows a light pink flower.",
+	"regrow":false,
+	"seasons":{
+		"spring":"good",
+		"summer":"normal",
+		"autumn":"normal",
+		"winter":"bad"
+	},
+	"category":"Seed",
+	"pricemultiplier":"Sd",
+	"cookingeffects":{
+		"hp":5
+	}
+})
