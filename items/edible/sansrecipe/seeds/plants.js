86395c6093dc069ce0646bc75ce7c9994eece494
glowfire <kunchyi1@gmail.com>
2016-07-16 18:56:31 +0800

    Delete plants.js

--- items/edible/sansrecipe/seeds/plants.js
+++
@@ -1,2 +0,0 @@
-// Toyherb, Pink Cat, Moondrop, Charm Blue, Cherry Grass, Pom-Pom Grass, Lamp Grass, Ironleaf, Clover, Autumn Grass, Noel Grass,
-// Fireflower, Emery Flower, Blue, Green, Red, White Crystal, in that order.
