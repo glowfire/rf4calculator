70e4b68c9a7facdcd18e05b8b259753920f55c66
glowfire <kunchyi1@gmail.com>
2016-07-16 20:58:02 +0800

    Update master.js

--- items/edible/sansrecipe/plants/nongrowable/master.js
+++ items/edible/sansrecipe/plants/nongrowable/master.js
@@ -1,19 +1,1 @@
 
-
-registerItem({
-	"name":"Bamboo Sprout",
-	"category":"Vegetable",
-	"sell":40,
-	"buy":200,
-	"pricemultiplier":"E",
-	"effects":{
-		"rp":8
-	},
-	"upgradeinfo":{
-		"rarity":0,
-		"difficulty":1,
-		"atk":1
-	},
-	"flavourtext":"Known for its crunchy texture and fragrant allure. Grows quickly during spring.",
-	"cookingeffects":{}
-})
