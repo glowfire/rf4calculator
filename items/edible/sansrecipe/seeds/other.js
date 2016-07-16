0680ac9e0b6d8079e953d9dd7a1bc264f19b11d9
glowfire <kunchyi1@gmail.com>
2016-07-16 18:58:25 +0800

    Update other.js

--- items/edible/sansrecipe/seeds/other.js
+++ items/edible/sansrecipe/seeds/other.js
@@ -1,24 +1,2 @@
 // Strawberry, Fodder, Shining, Orange, Pineapple,Grape, Apple, Dungeon, Sword, Shield
 
-registerItem({
-	"name":"Strawberry Seeds",
-	"sell":40,
-	"buy":400,
-	"value":135,
-	"growth":16,
-	"harvested":6,
-	"flavourtext":"Grows well in spring and fall. Bears sweet fruits. Hearty ones may grow again after harvesting.",
-	"regrow":true,
-	"seasons":{
-		"spring":"good",
-		"summer":"normal",
-		"autumn":"good",
-		"winter":"normal"
-	},
-	"category":"Seed",
-	"pricemultiplier":"Sd",
-	"cookingeffects":{
-		"hp":5
-	}
-})
-
