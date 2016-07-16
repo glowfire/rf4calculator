66f1814f48f6d1e123cccb0c4dc96d8eabb92f42
glowfire <kunchyi1@gmail.com>
2016-07-17 00:07:04 +0800

    Update categories.js

--- items/other/categories.js
+++ items/other/categories.js
@@ -3,6 +3,125 @@
 registerItem({
 	"name":"Squid (Category)",
 	"category":"Material",
-	"flavourtext":"A mollusk that's active at night. Uses the fins on its body to swim like it's flying in the water."
-	"members":["Squid","Sunsquid"]
+	"flavourtext":"A mollusk that's active at night. Uses the fins on its body to swim like it's flying in the water.",
+	"members":["Squid","Sunsquid"] /////////////////////////////////// needs manual input
+})
+
+registerItem({
+	"name":"Curry",
+	"category":"Material",
+	"flavourtext":"Staple dishes often served in households such as <q>Curry Rice</q> and <q>Dry Curry.</q>",
+	"members":["Curry Rice","Dry Curry"] //////////////////////////////// needs manual input
+})
+
+registerItem({
+	"name":"Jam",
+	"category":"Material",
+	"flavourtext":"Jam made by boiling fruits. Usually eaten with bread.",
+	"members":[]////////////////////////////////////////////////// needs manual input
+})
+
+registerItem({
+	"name":"Liquids",
+	"category":"Material",
+	"flavourtext":"Shapeless matter that is used as a material.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Powders and Spores",
+	"category":"Material",
+	"flavourtext":"Powders that mostly have magic power in them such as <q>Spores</q> and <q>Fairy Dust.</q>",
+	"members":[]
+})
+
+registerItem({
+	"name":"Minerals",
+	"category":"Material",
+	"flavourtext":"Used as materials to create tools, weapons, and armor. Mainly collected by hitting rocks with a hammer.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Jewels",
+	"category":"Material",
+	"flavourtext":"Precious stones with magic power such as <q>Amethysts</q> and <q>Aquamarine.</q> Mostly collected from colored minerals.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Claws and Fangs",
+	"category":"Material",
+	"flavourtext":"Monsters' body parts used to attack foes such as <q>Insect Jaw</q> and <q>Panther Claws.</q> Often used to reinforce weapons.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Crystals",
+	"category":"Material",
+	"flavourtext":"Substances made of runes that have crystalized. Often imbued with magic power.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Sticks and Stems",
+	"category":"Material",
+	"flavourtext":"Often used as a shaft or handle.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Scales",
+	"category":"Material",
+	"flavourtext":"Monster scales that are very hard and precious such as <q>Wet Scales</q> and <q>Dragon Scales.</q>",
+	"members":[]
+})
+
+registerItem({
+	"name":"Strings",
+	"category":"Material",
+	"flavourtext":"Long cotton-like materials used to tie things down or connect.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Feathers",
+	"category":"Material",
+	"flavourtext":"Fluffy, soft and downy feathers.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Shards",
+	"category":"Material",
+	"flavourtext":"Shards that were once a part of something such as <q>Arrowheads</q> and <q>Blade Shards.</q>",
+	"members":[]
+})
+
+registerItem({
+	"name":"Cloths and Skins",
+	"category":"Material",
+	"flavourtext":"Soft and plate-shaped substances for making clothes such as <q>Insect Carapace</q> and <q>Cheap Cloth.</q>",
+	"members":[]
+})
+
+registerItem({
+	"name":"Shells and Bones",
+	"category":"Material",
+	"flavourtext":"Because of their overall solidness, they're very useful as armor.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Stones",
+	"category":"Material",
+	"flavourtext":"Stones used as materials. Nobody really cares about them, but they're useful.",
+	"members":[]
+})
+
+registerItem({
+	"name":"Furs",
+	"category":"Material",
+	"flavourtext":"Monster furs and such that are soft and strong.",
+	"members":[]
 })
