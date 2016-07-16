ac0541e0d718cfb5ff6ed5da003c175d3bc18a73
glowfire <kunchyi1@gmail.com>
2016-07-16 20:03:57 +0800

    Delete master.js

--- items/edible/sansrecipe/fruits/master.js
+++
@@ -1,3 +0,0 @@
-// There are 8 [Fruit] items in the game: Mealy Apple, (Sultan) Strawberry, (King) Pineapple, Orange, Grapes and Apple. This section is complete.
-
-var fruitCategory = ["Mealy Apple","Strawberry","Sultan Strawberry","Pineapple","King Pineapple","Orange","Grapes","Apple"];
