cc757c1c70e8f9112471b684f9991ec573cd6303
glowfire <kunchyi1@gmail.com>
2016-07-16 17:26:49 +0800

    Update master.js

--- items/edible/sansrecipe/plants/growable/master.js
+++ items/edible/sansrecipe/plants/growable/master.js
@@ -1,3 +1,4 @@
+// Copmlete
 // Toyherb, Pink Cat, Moondrop, Charm Blue, Cherry Grass, Pom-Pom Grass, Lamp Grass, Ironleaf, Clover, Autumn Grass, Noel Grass,
 // Fireflower, Emery Flower, Blue, Green, Red, White Crystal
 
