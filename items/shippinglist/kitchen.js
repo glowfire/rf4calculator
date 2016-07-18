6c575b80c90c817380b8d557e993d33aa439638e
glowfire <kunchyi1@gmail.com>
2016-07-18 14:50:15 +0800

    Create kitchen.js

---
+++ items/shippinglist/kitchen.js
@@ -0,0 +1,12 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the sixth section.
+
+registerItem({
+	"name":"Rice Flour",
+	"category":"Ingredient",
+	"sell":40,
+	"buy":100,
+	"effects":{},
+	"flavourtext":"Powder used to make rice dumplings. Adds stickiness to the resulting dish.",
+	"pricemultiplier":"Ch"
+})
