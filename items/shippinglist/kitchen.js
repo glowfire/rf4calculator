b25a955f7e97be0c490b4e697d6ff7647566ed67
glowfire <kunchyi1@gmail.com>
2016-07-20 00:06:50 +0800

    Update kitchen.js

--- items/shippinglist/kitchen.js
+++ items/shippinglist/kitchen.js
@@ -590,6 +590,216 @@ registerItem({
 })
 
 registerItem({
+	"name":"Dumplings",
+	"cookinglevel":2,
+	"category":"Steamed",
+	"sell":45,
+	"buy":150,
+	"effects":{
+		"hp":200,
+		"rp":50,
+		"slpresP":70
+	},
+	"flavourtext":"Small rice cakes on skewers. Traditional dessert eaten during festivals.",
+	"materials":["Rice Flour"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Pumpkin Flan",
+	"cookinglevel":48,
+	"sell":1350,
+	"buy":5250,
+	"effects":{
+		"hp":5e3,
+		"rp":1200,
+		"ftgresP":100,
+		"sickresP":100
+	},
+	"flavourtext":"A flan with the sweet and savory flavor of pumpkin.",
+	"materials":["Egg (S)","Milk (S)","Pumpkin"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Flan",
+	"cookinglevel":29,
+	"sell":832,
+	"buy":2500,
+	"effects":{
+		"hp":3360,
+		"rp":600,
+		"ftgresP":75,
+		"sickresP":75
+	},
+	"flavourtext":"A jiggly dessert with a smooth texture. Shake it a little and watch it dance!",
+	"materials":["Egg (S)","Milk (S)"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Chocolate Sponge",
+	"cookinglevel":35,
+	"sell":1800,
+	"buy":5600,
+	"effects":{
+		"hp":4200,
+		"rp":800,
+		"slpresP":100
+	},
+	"flavourtext":"Chocolate cake that's as light and soft as a cloud.",
+	"materials":["Flour","Chocolate","Egg (S)","Milk (S)"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Pound Cake",
+	"cookinglevel":23,
+	"sell":608,
+	"buy":1880,
+	"effects":{
+		"hp":2580,
+		"rp":500,
+		"parresP":70
+	},
+	"flavourtext":"Square-shaped sponge cake. Steaming it brings out the sweet deliciousness.",
+	"materials":["Flour","Egg (S)"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Steamed Gyoza",
+	"cookinglevel":90,
+	"sell":9e3,
+	"buy":32e3,
+	"effects":{
+		"hp":5000,
+		"hpP":100,
+		"rp":1000,
+		"rpP":55,
+		"darkresP":50,
+		"psnresP":100,
+		"sealresP":100,
+		"parresP":100,
+		"slpresP":100,
+		"ftgresP":100,
+		"sickresP":100,
+		"faintresP":100
+	},
+	"flavourtext":"One of the common ways to cook potstickers. Juicy and savory.",
+	"materials":["Flour","Onion","Cabbage"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Curry Manju",
+	"cookinglevel":57,
+	"category":"Steamed",
+	"sell":4800,
+	"buy":36800,
+	"effects":{
+		"hp":5e3,
+		"hpP":47,
+		"rp":1e3,
+		"fireresP":10,
+		"darkresP":10,
+		"faintresP":100
+	},
+	"flavourtext":"A soft dumpling filled with spices.",
+	"materials":["Flour","Curry"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Chinese Manju",
+	"cookinglevel":15,
+	"category":"Steamed",
+	"sell":272,
+	"buy":820,
+	"effects":{
+		"hp":1260,
+		"rp":300,
+		"darkresP":5,
+		"psnresP":70
+	},
+	"flavourtext":"Soft bun filled with various ingredients and steamed.",
+	"materials":["Flour","Bamboo Sprout"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Meat Dumpling",
+	"cookinglevel":82,
+	"sell":5200,
+	"buy":16e3,
+	"effects":{
+		"hp":5000,
+		"hpP":100,
+		"rp":1000,
+		"rpP":31,
+		"psnresP":100,
+		"sealresP":100,
+		"parresP":100
+	},
+	"flavourtext":"A type of dim sum. Has a lovely sweet flavor and soft texture.",
+	"materials":["Flour","Shrimp","Onion"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Cheese Bread",
+	"cookinglevel":44,
+	"sell":2420,
+	"buy":7300,
+	"effects":{
+		"hp":5000,
+		"rp":1120,
+		"psnresP":50,
+		"sealresP":50,
+		"parresP":50,
+		"slpresP":50,
+		"faintresP":25
+	},
+	"flavourtext":"Steamed cheese bread. Moist and chewy, with a subtle scent of feet.",
+	"materials":["Flour","Cheese","Egg (S)","Milk (S)"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
+	"name":"Steamed Bread",
+	"cookinglevel":32,
+	"sell":1240,
+	"buy":3820,
+	"effects":{
+		"hp":3720,
+		"rp":800,
+		"sealresP":75
+	},
+	"flavourtext":"Bread that's been steamed. Makes a good snack for kids.",
+	"materials":["Flour","Egg (S)","Milk (S)"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Steamed"
+})
+
+registerItem({
 	"name":"Toast",
 	"category":"Oven",
 	"sell":170,
