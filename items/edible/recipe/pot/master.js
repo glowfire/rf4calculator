2c63a1c3f0cdcc589234247bff2da33414529767
glowfire <kunchyi1@gmail.com>
2016-07-20 01:54:41 +0800

    Update master.js

--- items/edible/recipe/pot/master.js
+++ items/edible/recipe/pot/master.js
@@ -1,4 +1,107 @@
 
+
+registerItem({
+	"name":"Rice Porridge",
+	"sell":57,
+	"buy":190,
+	"effects":{
+		"heal":["SICK"],
+		"hp":100,
+		"rp":150,
+		"rpmax":10,
+		"sickresP":20
+	},
+	"flavourtext":"Made by boiling rice. It's good for you, so stop complaining and just eat it already.",
+	"cookinglevel":2,
+	"materials":["Rice"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
+registerItem({
+	"name":"Marmalade",
+	"sell":90,
+	"buy":280,
+	"effects":{
+		"hp":30,
+		"rp":10
+	},
+	"flavourtext":"Jam made by boiling oranges. You can eat the orange skin too!",
+	"cookinglevel":5,
+	"materials":["Orange"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
+registerItem({
+	"name":"Udon",
+	"sell":145,
+	"buy":450,
+	"effects":{
+		"hp":300,
+		"rp":100,
+		"hpmax":150,
+		"earthresP":5
+	},
+	"flavourtext":"Thick and firm noodles. Very smooth and easy to eat.",
+	"cookinglevel":7,
+	"materials":["Flour"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
+registerItem({
+	"name":"Grape Jam",
+	"sell":135,
+	"buy":420,
+	"effects":{
+		"hp":30,
+		"rp":10
+	},
+	"flavourtext":"Jam made by boiling grapes. Generally eaten with bread.",
+	"cookinglevel":9,
+	"materials":["Grapes"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
+registerItem({
+	"name":"Apple Jam",
+	"sell":170,
+	"buy":520,
+	"effects":{
+		"hp":30,
+		"rp":10
+	},
+	"flavourtext":"Jam made by boiling apples. Generally eaten with bread.",
+	"cookinglevel":10,
+	"materials":["Grapes"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
+registerItem({
+	"name":"Hot Chocolate",
+	"sell":175,
+	"buy":530,
+	"effects":{
+		"hp":150,
+		"rp":500,
+		"rpmax":50
+	},
+	"flavourtext":"A sweet drink of melted chocolate. Be careful not to get cavities!",
+	"cookinglevel":11,
+	"materials":["Chocolate"],
+	"cookingeffects":{},
+	"pricemultiplier":"C1",
+	"category":"Pot"
+})
+
 registerItem({
 	"name":"Boiled Spinach",
 	"cookinglevel":13,
