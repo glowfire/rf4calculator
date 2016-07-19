55a4d51aa7dd72556d164b3fb7a82757e24947cc
glowfire <kunchyi1@gmail.com>
2016-07-19 23:36:22 +0800

    Create steamer.js

---
+++ items/edible/recipe/steamer.js
@@ -0,0 +1,18 @@
+
+
+registerItem({
+	"name":"Dumplings",
+	"cookinglevel":2,
+	"category":"Steamed",
+	"sell":45,
+	"buy":150,
+	"effects":{
+		"hp":200,
+		"rp":50,
+		"slpresP":70
+	},
+	"flavourtext":"Small rice cakes on skewers. Traditional dessert eaten during festivals.",
+	"materials":["Rice Flour"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{}
+})
