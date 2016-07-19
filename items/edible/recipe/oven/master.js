13d46b6aa2f6e9c572d82f441e817f0d0116cc82
glowfire <kunchyi1@gmail.com>
2016-07-19 23:28:50 +0800

    Update master.js

--- items/edible/recipe/oven/master.js
+++ items/edible/recipe/oven/master.js
@@ -15,7 +15,25 @@ registerItem({
 		"str":4,
 		"vit":4,
 		"int":4
-	}
+	},
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Baked Onigiri",
+	"cookinglevel":10,
+	"category":"Oven",
+	"sell":65,
+	"buy":220,
+	"pricemultiplier":"C1",
+	"flavourtext":"A <q>rice ball</q> baked on both sides. Has an addictively crunchy texture.",
+	"effects":{
+		"hp":500,
+		"rp":200,
+		"vit":5
+	},
+	"materials":["Onigiri"],
+	"cookingeffects":{}
 })
 
 registerItem({
@@ -31,7 +49,8 @@ registerItem({
 		"hp":2020,
 		"rp":500,
 		"vit":10
-	}
+	},
+	"cookingeffects":{}
 })
 
 registerItem({
@@ -47,7 +66,78 @@ registerItem({
 		"hp":2040,
 		"rp":800,
 		"vit":15
-	}
+	},
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Raisin Bread",
+	"cookinglevel":27,
+	"category":"Oven",
+	"sell":380,
+	"buy":1200,
+	"pricemultiplier":"C1",
+	"flavourtext":"Bread with raisins in it. Soft, sweet and sour, all at once.",
+	"effects":{
+		"hp":3000,
+		"rp":400,
+		"str":15,
+		"vit":8,
+		"int":10
+	},
+	"materials":["Flour","Grapes"],
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Jam Roll",
+	"cookinglevel":34,
+	"category":"Oven",
+	"sell":1940,
+	"buy":5000,
+	"pricemultiplier":"C1",
+	"flavourtext":"Bread baked with jam. Can be eaten as a snack.",
+	"effects":{
+		"hp":3760,
+		"rp":1000,
+		"vit":25
+	},
+	"materials":["Flour","Jam","Egg (S)","Milk (S)"],
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Apple Pie",
+	"cookinglevel":36,
+	"category":"Oven",
+	"sell":1910,
+	"buy":5820,
+	"pricemultiplier":"C1",
+	"flavourtext":"Apples baked in a pie crust. Popular as a dessert and snack alike.",
+	"effects":{
+		"hp":4240,
+		"rp":1000,
+		"vit":30
+	},
+	"materials":["Flour","Apple","Butter","Egg (S)"],
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Cookie",
+	"cookinglevel":38,
+	"category":"Oven",
+	"sell":1960,
+	"buy":5980,
+	"pricemultiplier":"C1",
+	"flavourtext":"Confection made from flour and dough. Crisp and heartwarming. Makes a great gift!",// could be comma after heartwarming
+	"effects":{
+		"hp":4560,
+		"rp":1000,
+		"vit":35
+	},
+	"materials":["Flour","Sweet Powder","Egg (S)","Butter"],
+	"cookingeffects":{}
 })
 
 registerItem({
