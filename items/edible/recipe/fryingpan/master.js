311739a4ca997f3e81d6f0f0fa3d1fa1f4b73ff0
glowfire <kunchyi1@gmail.com>
2016-07-20 23:59:08 +0800

    Update master.js

--- items/edible/recipe/fryingpan/master.js
+++ items/edible/recipe/fryingpan/master.js
@@ -1,6 +1,101 @@
 
 
 registerItem({
+	"name":"Baked Apple",
+	"sell":175,
+	"buy":550,
+	"effects":{
+		"hp":550,
+		"hpmax":10,
+		"str":1,
+		"vit":1,
+		"int":1
+	},
+	"flavourtext":"An apple-based dessert item. Good mix of sweet and tart.",
+	"cookinglevel":9,
+	"materials":["Apple"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Fried Eggs",
+	"sell":335,
+	"buy":1100,
+	"effects":{
+		"hp":2360,
+		"hpmax":50,
+		"str":5,
+		"vit":5,
+		"int":5
+	},
+	"flavourtext":"Typical home-cooked egg dish. How long they should be fried is up to you!",
+	"cookinglevel":20,
+	"materials":["Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"French Fries",
+	"sell":614,
+	"buy":2e3,
+	"effects":{
+		"hp":3160,
+		"hpmax":80,
+		"str":7,
+		"vit":7,
+		"int":7
+	},
+	"flavourtext":"Peeled and sliced potatoes fried in oil until they're crisp. Hard to resist.",
+	"cookinglevel":24,
+	"materials":["Oil","Potato"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Omelet",
+	"sell":820,
+	"buy":2500,
+	"effects":{
+		"hp":3800,
+		"hpmax":100,
+		"str":10,
+		"vit":10,
+		"int":10
+	},
+	"flavourtext":"Typical breakfast item eaten around the world. Why is this? Have you met a person who doesn't like omelets? No? Well there you go.",
+	"cookinglevel":28,
+	"materials":["Milk (S)","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Fried Rice",
+	"sell":1080,
+	"buy":3300,
+	"effects":{
+		"hp":4120,
+		"hpmax":150,
+		"str":12,
+		"vit":12,
+		"int":12
+	},
+	"flavourtext":"Rice and egg fried in oil. The sweet, fluffy egg makes it extra tasty.",
+	"cookinglevel":30,
+	"materials":["Rice","Oil","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
 	"name":"Miso Eggplant",
 	"category":"Frying Pan",
 	"cookinglevel":31,
@@ -15,7 +110,123 @@ registerItem({
 		"vit":13,
 		"int":13
 	},
-	"materials":["Eggplant"]
+	"materials":["Eggplant"],
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"French Toast",
+	"sell":670,
+	"buy":3e3,
+	"effects":{
+		"hp":4280,
+		"hpmax":200,
+		"str":15,
+		"vit":15,
+		"int":15
+	},
+	"flavourtext":"Bread fried with milk and egg. Mmmm, such a delicate texture! Like a work of art...",
+	"cookinglevel":31,
+	"materials":["Bread","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Croquettes",
+	"sell":1300,
+	"buy":4e3,
+	"effects":{
+		"hp":4600,
+		"hpmax":250,
+		"str":17,
+		"vit":17,
+		"int":17
+	},
+	"flavourtext":"Mashed potatoes packed together and fried in oil. Widely beloved as comfort food.",
+	"cookinglevel":33,
+	"materials":["Oil","Potato","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Tempura",
+	"sell":1820,
+	"buy":5600,
+	"effects":{
+		"hp":5160,
+		"hpmax":270,
+		"str":20,
+		"vit":20,
+		"int":20
+	},
+	"flavourtext":"Food battered in flour and egg that is later deep fried. A somewhat generic name for a whole category of yumminess.",
+	"cookinglevel":36,
+	"materials":["Oil","Flour","Shrimp","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Donut",
+	"sell":2450,
+	"buy":7500,
+	"effects":{
+		"hp":6440,
+		"hpmax":300,
+		"str":26,
+		"vit":26,
+		"int":26
+	},
+	"flavourtext":"Flour and butter mixed and fried in a convenient ring shape.",
+	"cookinglevel":46,
+	"materials":["Oil","Flour","Butter","Egg (M)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Risotto",
+	"sell":2300,
+	"buy":7e3,
+	"effects":{
+		"hp":6680,
+		"hpmax":400,
+		"str":30,
+		"vit":30,
+		"int":30
+	},
+	"flavourtext":"Rice mixed with sauce to create perfect harmony.",
+	"cookinglevel":48,
+	"materials":["Rice","Wine","Butter"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Pancakes",
+	"sell":3120,
+	"buy":9500,
+	"effects":{
+		"hp":5e3,
+		"rp":1840,
+		"hpmax":500,
+		"str":35,
+		"vit":35,
+		"int":35
+	},
+	"flavourtext":"Eaten for breakfast or as a snack. Possible toppings are limited only by your imagination.",
+	"cookinglevel":49,
+	"materials":["Flour","Honey","Milk (S)","Egg (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
 })
 
 registerItem({
@@ -35,21 +246,90 @@ registerItem({
 		"vit":40,
 		"int":40
 	},
-	"materials":["Corn"]
+	"materials":["Corn"],
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Corn Cereal",
+	"sell":2800,
+	"buy":9200,
+	"effects":{
+		"hp":5e3,
+		"hpP":29,
+		"rp":1e3,
+		"hpmax":800,
+		"str":50,
+		"vit":50,
+		"int":50
+	},
+	"flavourtext":"Corn flattened and baked. A popular breakfast food.",
+	"cookinglevel":53,
+	"materials":["Corn","Milk (S)"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Curry Bread",
+	"sell":6e3,
+	"buy":36e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":56,
+		"rp":1e3,
+		"hpmax":800,
+		"hpmaxP":1,
+		"str":50,
+		"strP":1,
+		"vit":50,
+		"vitP":1,
+		"int":50,
+		"intP":1,
+		"fireresP":10
+	},
+	"flavourtext":"Bread with curry inside. Crispy on the outside, soft on the inside. Perfect harmony!",
+	"cookinglevel":59,
+	"materials":["Oil","Flour","Curry"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Omelet Rice",
+	"sell":5200,
+	"buy":16e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":62,
+		"rp":1e3,
+		"hpmax":800,
+		"hpmaxP":3,
+		"str":50,
+		"strP":2,
+		"vit":50,
+		"vitP":2,
+		"int":50,
+		"intP":2
+	},
+	"flavourtext":"Fried rice flavored with ketchup and wrapped in a thin omelet.",
+	"cookinglevel":60,
+	"materials":["Omelet","Rice","Ketchup"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
 })
 
 registerItem({
 	"name":"Fried Veggies",
-	"category":"Frying Pan",
-	"cookinglevel":62,
 	"sell":1550,
-	"buy":5000,
-	"pricemultiplier":"C1",
-	"flavourtext":"Various vegetables fried together. Much better than eating them raw. But anyone could have told you that- frying makes anything better!",
+	"buy":5e3,
 	"effects":{
-		"hp":5000,
+		"hp":5e3,
 		"hpP":71,
-		"rp":1000,
+		"rp":1e3,
 		"hpmax":800,
 		"hpmaxP":3,
 		"str":50,
@@ -59,6 +339,117 @@ registerItem({
 		"int":50,
 		"intP":3
 	},
-	"materials":["Cabbage"]
+	"flavourtext":"Various vegetables fried together. Much better than eating them raw. But anyone could have told you that- frying makes anything better!",
+	"cookinglevel":62,
+	"materials":["Cabbage"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Cabbage Cakes",
+	"sell":6e3,
+	"buy":23600,
+	"effects":{
+		"hp":5e3,
+		"hpP":77,
+		"rp":1e3,
+		"hpmax":800,
+		"hpmaxP":4,
+		"str":50,
+		"strP":4,
+		"vit":50,
+		"vitP":4,
+		"int":50,
+		"intP":4
+	},
+	"flavourtext":"Water, flour and cabbage mixed and baked like a pancake. Recipes vary from region to region.",
+	"cookinglevel":64,
+	"materials":["Flour","Cabbage","Egg(S)","Leek"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Dry Curry",
+	"sell":8600,
+	"buy":49800,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":43,
+		"hpmax":800,
+		"hpmaxP":5,
+		"str":50,
+		"strP":5,
+		"vit":50,
+		"vitP":5,
+		"int":50,
+		"intP":5,
+		"fireresP":15
+	},
+	"flavourtext":"Unlike normal curry, it's more a powder than a sauce. Can be eaten with rice or bread.",
+	"cookinglevel":86,
+	"materials":["Rice","Curry Powder","Carrot","Onion"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Gyoza",
+	"sell":9e3,
+	"buy":32e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":49,
+		"hpmax":800,
+		"hpmaxP":8,
+		"str":50,
+		"strP"6:,
+		"vit":50,
+		"vitP":6,
+		"int":50,
+		"intP":6,
+		"darkresP":10
+	},
+	"flavourtext":"Meat and vegetables wrapped in thin dough and fried. Can also be steamed or boiled.",
+	"cookinglevel":88,
+	"materials":["Flour","Onion","Cabbage"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Fried Udon",
+	"sell":12e3,
+	"buy":5e4,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":70,
+		"hpmax":1e3,
+		"hpmaxP":10,
+		"str":70,
+		"strP":7,
+		"vit":70,
+		"vitP":7,
+		"int":70,
+		"intP":7,
+		"earthresP":50
+	},
+	"flavourtext":"Someone happened to try frying udon noodles. It turned out surprisingly well.",
+	"cookinglevel":95,
+	"materials":["Flour","Onion","Carrot","Cabbage"],
+	"preicemultiplier":"C1",
+	"category":"Frying Pan",
+	"cookingeffects":{}
 })
 	
