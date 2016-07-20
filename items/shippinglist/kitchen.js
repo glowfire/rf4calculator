e0b7b699f70686fc1a08bed9290e848573f3ae69
glowfire <kunchyi1@gmail.com>
2016-07-21 01:03:52 +0800

    Update kitchen.js

--- items/shippinglist/kitchen.js
+++ items/shippinglist/kitchen.js
@@ -3014,6 +3014,451 @@ registerItem({
 })
 
 registerItem({
+	"name":"Shrimp Sashimi",
+	"sell":125,
+	"buy":680,
+	"effects":{
+		"hp":1e3,
+		"rp":240,
+		"str":20
+	},
+	"flavourtext":"Freshly peeled shrimp. Eat it with soy sauce.",
+	"cookinglevel":14,
+	"materials":["Shrimp"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Lobster Sashimi",
+	"sell":1230,
+	"buy":3800,
+	"effects":{
+		"hp":5e3,
+		"rp":800,
+		"str":35
+	},
+	"flavourtext":"Freshly peeled lobster. Slightly sweet, and particularly popular among children.",
+	"cookinglevel":40,
+	"materials":["Lobster"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Blowfish Sashimi",
+	"sell":8800,
+	"buy":36e3,
+	"effects":{
+		"statusailments":["PAR"],
+		"hp":5e3,
+		"hpP":98,
+		"rp":1e3,
+		"str":150,
+		"strP":5,
+		"parresP":100
+	},
+	"flavourtext":"Thinly sliced fresh blowfish. So thin you can almost see through it!",
+	"cookinglevel":71,
+	"materials":["Blowfish"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Lamp Squid Sashimi",
+	"sell":2895,
+	"buy":12e3,
+	"effects":{
+		"statusailments":["SEAL"],
+		"hp":5e3,
+		"hpP":26,
+		"rp":1e3,
+		"sealresP":100
+	},
+	"flavourtext":"Freshly sliced lamp squid. Once you eat its lamp, your fate (and status) is sealed.",
+	"cookinglevel":53,
+	"materials":["Lamp Squid"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Sunsquid Sashimi",
+	"sell":11800,
+	"buy":64e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"str":150,
+		"strP":10
+	},
+	"flavourtext":"Passionately sliced sunsquid. Popular in certain regions.",
+	"cookinglevel":77,
+	"materials":["Sunsquid"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Squid Sashimi",
+	"sell":29,
+	"buy":300,
+	"effects":{
+		"hp":200,
+		"rp":80,
+		"str":10
+	},
+	"flavourtext":"Freshly sliced squid. Eat it with soy sauce.",
+	"cookinglevel":4,
+	"materials":["Squid"], // the item Squid, not the category
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Fall Sashimi",
+	"sell":3750,
+	"buy":15800,
+	"effects":{
+		"hp":5e3,
+		"hpP":50,
+		"rp":1e3,
+		"str":70
+	},
+	"flavourtext":"Rare fall flounder sashimi. Its edges are delicious and help maintain one's beautiful glow.",
+	"cookinglevel":58,
+	"materials":["Fall Flounder"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Turbot Sashimi",
+	"sell":150,
+	"buy":1300,
+	"effects":{
+		"hp":1900,
+		"rp":700,
+		"str":25
+	},
+	"flavourtext":"Freshly sliced turbot. The edges are very tasty.",
+	"cookinglevel":21,
+	"materials":["Turbot"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Flounder Sashimi",
+	"sell":300,
+	"buy":2300,
+	"effects":{
+		"hp":3240,
+		"rp":800,
+		"str":28
+	},
+	"flavourtext":"Freshly sliced flounder. The edges are the most valuable part.",
+	"cookinglevel":30,
+	"materials":["Flounder"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Pike Sashimi",
+	"sell":13,
+	"buy":150,
+	"effects":{
+		"hp":120,
+		"rp":30,
+		"str":1
+	},
+	"flavourtext":"Freshly sliced pike. Eat it with soy sauce.",
+	"cookinglevel":1,
+	"materials":["Pike"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Needlefish Sashimi",
+	"sell":34,
+	"buy":300,
+	"effects":{
+		"hp":200,
+		"rp":70,
+		"str":7
+	},
+	"flavourtext":"Freshly sliced needlefish. Eat it with soy sauce.",
+	"cookinglevel":4,
+	"materials":["Needlefish"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Sardine Sashimi",
+	"sell":88,
+	"buy":500,
+	"effects":{
+		"hp":359,
+		"rp":100,
+		"str":15
+	},
+	"flavourtext":"Freshly sliced sardine. Eat it with soy sauce.",
+	"cookinglevel":8,
+	"materials":["Sardine"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Tuna Sashimi",
+	"sell":9500,
+	"buy":48e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"str":150,
+		"strP":7
+	},
+	"flavourtext":"The most expensive and delicious part of tuna. A true delicacy!",
+	"cookinglevel":74,
+	"materials":["Tuna"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Yellowtail Sashimi",
+	"sell":6700,
+	"buy":27e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":89,
+		"rp":1e3,
+		"str":150,
+		"strP":3
+	},
+	"flavourtext":"Freshly sliced yellowtail. Fatty and rich in taste.",
+	"cookinglevel":68,
+	"materials":["Yellowtail"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Skipjack Sashimi",
+	"sell":300,
+	"buy":3400,
+	"effects":{
+		"hp":4520,
+		"rp":800,
+		"str":30
+	},
+	"flavourtext":"Freshly sliced skipjack. Often eaten with ginger.",
+	"cookinglevel":37,
+	"materials":["Skipjack"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Girella Sashimi",
+	"sell":52,
+	"buy":400,
+	"effects":{
+		"hp":220,
+		"rp":100,
+		"str":12
+	},
+	"flavourtext":"Freshly sliced girella. Eat it with soy sauce.",
+	"cookinglevel":6,
+	"materials":["Girella"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Lover Sashimi",
+	"sell":5400,
+	"buy":21800,
+	"effects":{
+		"hp":5e3,
+		"hpP":74,
+		"rp":1e3,
+		"str":99
+	},
+	"flavourtext":"Mmm, precious lover snapper! Eat without seasonings.",
+	"cookinglevel":63,
+	"materials":["Throbby Snapper"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Glitter Sashimi",
+	"sell":24e3,
+	"buy":12e4,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"str":150,
+		"strP":18,
+		"lightresP":50
+	},
+	"flavourtext":"Freshly sliced glitter snapper. Sweet, but prohibitively expensive due to its fabulousness.",
+	"cookinglevel":92,
+	"materials":["Glitter Snapper"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Snapper Sashimi",
+	"sell":1980,
+	"buy":8e3,
+	"effects":{
+		"hp":5800,
+		"rp":1e3,
+		"str":40
+	},
+	"flavourtext":"Freshly sliced snapper. A symbol of good luck.",
+	"cookinglevel":50,
+	"materials":["Snapper"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Taimen Sashimi",
+	"sell":12200,
+	"buy":68e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"str":150,
+		"strP":15
+	},
+	"flavourtext":"Freshly sliced taimen. Eat it with soy sauce.",
+	"cookinglevel":79,
+	"materials":["Taimen"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Cherry Sashimi",
+	"sell":21,
+	"buy":240,
+	"effects":{
+		"hp":200,
+		"rp":50,
+		"str":5
+	},
+	"flavourtext":"Freshly sliced cherry salmon. Eat it with soy sauce.",
+	"cookinglevel":3,
+	"materials":["Cherry Salmon"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Salmon Sashimi",
+	"sell":100,
+	"buy":570,
+	"effects":{
+		"hp":600,
+		"rp":150,
+		"str":18
+	},
+	"flavourtext":"Freshly sliced salmon. Eat it with soy sauce.",
+	"cookinglevel":12,
+	"materials":["Salmon"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Rainbow Sashimi",
+	"sell":240,
+	"buy":1740,
+	"effects":{
+		"statusailments":["PSN"],
+		"hp":2860,
+		"rp":700,
+		"psnresP":100
+	},
+	"flavourtext":"Freshly sliced rainbow trout. Eat it with soy sauce. But beware! It may still have poison in it...",
+	"cookinglevel":26,
+	"materials":["Rainbow Trout"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Char Sashimi",
+	"sell":130,
+	"buy":720,
+	"effects":{
+		"hp":1e3,
+		"rp":400,
+		"str":22
+	},
+	"flavourtext":"Freshly sliced char. Eat it with soy sauce.",
+	"cookinglevel":15,
+	"materials":["Char"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
+	"name":"Trout Sashimi",
+	"sell":17,
+	"buy":210,
+	"effects":{
+		"hp":150,
+		"rp":50,
+		"str":3
+	},
+	"flavourtext":"Freshly sliced trout. Eat it with soy sauce.",
+	"cookinglevel":2,
+	"materials":["Masu Trout"],
+	"pricemultiplier":"C1",
+	"category":"Knife",
+	"cookingeffects":{}
+})
+
+registerItem({
 	"name":"Disastrous Dish",
 	"category":"Failed Dish",
 	"sell":50,
