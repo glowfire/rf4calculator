04f7065d8d6077bd22cc68c133aa594d9175f6c6
glowfire <kunchyi1@gmail.com>
2016-07-20 23:07:08 +0800

    Update kitchen.js

--- items/shippinglist/kitchen.js
+++ items/shippinglist/kitchen.js
@@ -1983,6 +1983,584 @@ registerItem({
 })
 
 registerItem({
+	"name":"Grilled S. Flounder",
+	"sell":275,
+	"buy":850,
+	"effects":{
+		"hp":1480,
+		"rpmax":25,
+		"int":12,
+		"psnatkP":1,
+		"paratkP":1
+	},
+	"flavourtext":"Salted and grilled sand flounder. You can almost taste the sand.",
+	"cookinglevel":15,
+	"materials":["Sand Flounder","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Shrimp",
+	"sell":398,
+	"buy":1200,
+	"effects":{
+		"hp":1960,
+		"rpmax":45,
+		"int":20
+	},
+	"flavourtext":"Whole shrimp, seasoned and grilled. People love its crispy texture.",
+	"cookinglevel":18,
+	"materials":["Shrimp","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Lobster",
+	"sell":1720,
+	"buy":5200,
+	"effects":{
+		"hp":5880,
+		"rpmax":105,
+		"int":37
+	},
+	"flavourtext":"A whole lobster, seasoned and grilled. Stands out on the dinner table.",
+	"cookinglevel":41,
+	"materials":["Lobster","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Blowfish",
+	"sell":7800,
+	"buy":48e3,
+	"effects":{
+		"statusailments":["PAR"],
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":1,
+		"rpmax":200,
+		"rpmaxP":5,
+		"int":80,
+		"intP":3,
+		"parresP":100,
+		"paratkP":50
+	},
+	"flavourtext":"An uncommon dish. Most people wouldn't think to grill blowfish, y'know?",
+	"cookinglevel":72,
+	"materials":["Blowfish","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Lamp Squid",
+	"sell":3780,
+	"buy":15200,
+	"effects":{
+		"statusailments":["SEAL"],
+		"hp":5e3,
+		"hpP":32,
+		"rp":1e3,
+		"rpmax":150,
+		"int":45,
+		"sealresP":100,
+		"sealatkP":50
+	},
+	"flavourtext":"A little bitter when grilled, but nonetheless a popular dish among foodies. Perhaps they just like being sealed?",
+	"cookinglevel":54,
+	"materials":["Lamp Squid","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Sunsquid",
+	"sell":15800,
+	"buy":8e4,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":19,
+		"rpmax":200,
+		"rpmaxP":8,
+		"int":100,
+		"intP":6
+	},
+	"flavourtext":"Dried and grilled sunsquid. Savory, but easily burned.",
+	"cookinglevel":78,
+	"materials":["Sunsquid","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Squid",
+	"sell":250,
+	"buy":800,
+	"effects":{
+		"hp":1080,
+		"rpmax":15,
+		"int":8
+	},
+	"flavourtext":"Dried and grilled squid. Makes a great snack when seasoned with chili pepper and mayonnaise.",
+	"cookinglevel":13,
+	"materials":["Squid","Sour Drop"], ////////////////////// Squid the category, not Squid the item.
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled F.Flounder",
+	"sell":4850,
+	"buy":2e4,
+	"effects":{
+		"hp":5e3,
+		"hpP":53,
+		"rp":1e3,
+		"rpmax":180,
+		"int":50
+	},
+	"flavourtext":"Salted and grilled fall flounder. Its beautiful colors grab attention.",
+	"cookinglevel":58,
+	"materials":["Fall Flounder","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Turbot",
+	"sell":648,
+	"buy":2e3,
+	"effects":{
+		"hp":2680,
+		"rpmax":60,
+		"int":25
+	},
+	"flavourtext":"Salted and grilled turbot. The fresher it is, the better it tastes.",
+	"cookinglevel":21,
+	"materials":["Turbot","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Flounder",
+	"sell":420,
+	"buy":3250,
+	"effects":{
+		"hp":4360,
+		"rpmax":90,
+		"int":33
+	},
+	"flavourtext":"Salted and grilled flounder. The fresher it is, the better it tastes.",
+	"cookinglevel":31,
+	"materials":["Flounder","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Pike",
+	"sell":190,
+	"buy":600,
+	"effects":{
+		"hp":480,
+		"rpmax":5,
+		"int":2
+	},
+	"flavourtext":"Salted and grilled pike. The fresher it is, the better it tastes.",
+	"cookinglevel":8,
+	"materials":["Pike","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Needlefish",
+	"sell":248,
+	"buy":750,
+	"effects":{
+		"hp":800,
+		"rpmax":10,
+		"int":5
+	},
+	"flavourtext":"Salted and grilled needlefish. The fresher it is, the better it tastes.",
+	"cookinglevel":12,
+	"materials":["Needlefish","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Dried Sardines",
+	"sell":400,
+	"buy":2300,
+	"effects":{
+		"hp":3240,
+		"rpmax":70,
+		"int":30
+	},
+	"flavourtext":"Skewered and dried sardines. Often grilled before eating.",
+	"cookinglevel":24,
+	"materials":["Sardine","Sardine","Sardine"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Tuna Teriyaki",
+	"sell":11e3,
+	"buy":61e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":10,
+		"rpmax":200,
+		"rpmaxP":6,
+		"int":90,
+		"intP":5
+	},
+	"flavourtext":"Tuna marinated in teriyaki sauce and fried. To make it shine, apply the sauce repeatedly while cooking.",
+	"cookinglevel":75,
+	"materials":["Tuna","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Pond Smelt",
+	"sell":173,
+	"buy":600,
+	"effects":{
+		"hp":350,
+		"rpmax":3,
+		"int":1
+	},
+	"flavourtext":"Salted and grilled pond smelt. The fresher it is, the better it tastes.",
+	"materials":["Pond Smelt","Sour Drop"],
+	"cookinglevel":6,
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Yellowtail",
+	"sell":8600,
+	"buy":35400,
+	"effects":{
+		"hp":5e3,
+		"hpP":92,
+		"rp":1e3,
+		"rpmax":200,
+		"rpmaxP":3,
+		"int":70,
+		"intP":2
+	},
+	"flavourtext":"Salted and grilled yellowtail. Sugar in the teriyaki sauce makes its surface shiny.",
+	"cookinglevel":69,
+	"materials":["Yellowtail","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Mackerel",
+	"sell":325,
+	"buy":1e3,
+	"effects":{
+		"hp":1720,
+		"rpmax":35,
+		"int":16
+	},
+	"flavourtext":"Salted and grilled mackerel. The fresher it is, the better it tastes.",
+	"cookinglevel":16,
+	"materials":["Mackerel","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Skipjack",
+	"sell":510,
+	"buy":4800,
+	"effects":{
+		"hp":5400,
+		"rpmax":100,
+		"int":35
+	},
+	"flavourtext":"Lightly grilled skipjack. Taste varies depending on its freshness.",
+	"cookinglevel":37,
+	"materials":["Skipjack","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled L. Snapper",
+	"sell":7100,
+	"buy":28800,
+	"effects":{
+		"hp":5e3,
+		"hpP":83,
+		"rp":1e3,
+		"rpmax":200,
+		"rpmaxP":2,
+		"int":60,
+		"intP":1
+	},
+	"flavourtext":"Salted and grilled lover snapper. Surprisingly sweet.",
+	"cookinglevel":66,
+	"materials":["Throbby Snapper","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled G. Snapper",
+	"sell":30500,
+	"buy":154e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":64,
+		"rpmax":200,
+		"rpmaxP":12,
+		"int":100,
+		"intP":10,
+		"lightresP":50
+	},
+	"flavourtext":"Salted and grilled glitter snapper. The fresher it is, the better it tastes.",
+	"cookinglevel":93,
+	"materials":["Glitter Snapper","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Girella",
+	"sell":295,
+	"buy":900,
+	"effects":{
+		"hp":1640,
+		"rpmax":30,
+		"int":15
+	},
+	"flavourtext":"Salted and grilled girella. The fresher it is, the better it tastes.",
+	"cookinglevel":16,
+	"materials":["Girella","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Snapper",
+	"sell":2680,
+	"buy":11200,
+	"effects":{
+		"hp":5e3,
+		"hpP":17,
+		"rp":1e3,
+		"rpmax":120,
+		"int":40
+	},
+	"flavourtext":"Salted and grilled snapper. The fresher it is, the better it tastes.",
+	"cookinglevel":51,
+	"materials":["Snapper","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled Gibelio",
+	"sell":272,
+	"buy":1800,
+	"effects":{
+		"hp":2280,
+		"rpmax":55,
+		"int":23
+	},
+	"flavourtext":"Salted and grilled gibelio. The fresher it is, the better it tastes.",
+	"cookinglevel":19,
+	"materials":["Gibelio","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Grilled C. Carp",
+	"sell":265,
+	"buy":810,
+	"effects":{
+		"hp":1320,
+		"rpmax":20,
+		"int":10
+	},
+	"flavourtext":"Salted and grilled crucian carp. The fresher it is, the better it tastes.",
+	"cookinglevel":14,
+	"materials":["Crucian Carp","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Taimen",
+	"sell":16e3,
+	"buy":88e3,
+	"effects":{
+		"hp":5e3,
+		"hpP":100,
+		"rp":1e3,
+		"rpP":25,
+		"rpmax":200,
+		"rpmaxP":10,
+		"int":100,
+		"intP":8
+	},
+	"flavourtext":"Salted and grilled taimen. The fresher it is, the better it tastes.",
+	"cookinglevel":80,
+	"materials":["Taimen","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Salmon",
+	"sell":370,
+	"buy":1200,
+	"effects":{
+		"hp":1800,
+		"rpmax":40,
+		"int":18
+	},
+	"flavourtext":"Salted and grilled salmon. The fresher it is, the better it tastes.",
+	"cookinglevel":17,
+	"materials":["Salmon","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Chub",
+	"sell":330,
+	"buy":2200,
+	"effects":{
+		"hp":3e3,
+		"rpmax":65,
+		"int":28
+	},
+	"flavourtext":"Salted and grilled chub. The fresher it is, the better it tastes.",
+	"cookinglevel":23,
+	"materials":["Chub","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted C. Salmon",
+	"sell":238,
+	"buy":730,
+	"effects":{
+		"hp":780,
+		"rpmax":8,
+		"int":4
+	},
+	"flavourtext":"Salted and grilled cherry salmon. The fresher it is, the better it tastes.",
+	"cookinglevel":12,
+	"materials":["Cherry Salmon","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted R. Trout",
+	"sell":390,
+	"buy":2750,
+	"effects":{
+		"statusailments":["PSN"],
+		"hp":3640,
+		"rpmax":80,
+		"int":32,
+		"psnresP":100,
+		"psnatkP":50
+	},
+	"flavourtext":"Salted and grilled rainbow trout. The fresher it is, the better it tastes.",
+	"cookinglevel":27,
+	"materials":["Rainbow Trout","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Char",
+	"sell":260,
+	"buy":1300,
+	"effects":{
+		"hp":2200,
+		"rpmax":50,
+		"int":22
+	},
+	"flavourtext":"Salted and grilled char. The fresher it is, the better it tastes.",
+	"cookinglevel":19,
+	"materials":["Char","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
+	"name":"Salted Masu Trout",
+	"sell":220,
+	"buy":680,
+	"effects":{
+		"hp":600,
+		"rpmax":7,
+		"int":3
+	},
+	"flavourtext":"Salted and grilled masu trout. The fresher it is, the better it tastes.",
+	"cookinglevel":9,
+	"materials":["Masu Trout","Sour Drop"],
+	"pricemultiplier":"C1",
+	"cookingeffects":{},
+	"category":"Frying Pan"
+})
+
+registerItem({
 	"name":"Disastrous Dish",
 	"category":"Failed Dish",
 	"sell":50,
