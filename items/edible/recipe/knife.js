52374162a4ce6352804ce189031b9f2708fe578b
glowfire <kunchyi1@gmail.com>
2016-07-21 00:22:47 +0800

    Update knife.js

--- items/edible/recipe/knife.js
+++ items/edible/recipe/knife.js
@@ -1,3 +1,18 @@
 
 
-registerItem({})
+registerItem({
+	"name":,
+	"category":"Knife",
+	"sell":,
+	"buy":,
+	"effects":{
+		"hp":,
+		"rp":,
+		"str":
+	},
+	"flavourtext":"",
+	"cookinglevel":,
+	"materials":[""],
+	"pricemultiplier":"C1",
+	"cookingeffects":{}
+})
