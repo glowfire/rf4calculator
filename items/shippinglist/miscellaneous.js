252bf1cb8f2ba8556594c4cd6092545f6e823011
glowfire <kunchyi1@gmail.com>
2016-07-21 15:06:52 +0800

    Create miscellaneous.js

---
+++ items/shippinglist/miscellaneous.js
@@ -0,0 +1,12 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the eighth section.
+
+registerItem({
+	"name":"Iron Waltz",
+	"category":"Special",
+	"sell":390,
+	"buy":7800,
+	"flavourtext":"A musical score that powers up allied monsters' defense until the end of the day.",
+	"pricemultiplier":"X" /// unknown
+	// cannot be used as material
+})
