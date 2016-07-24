67a8248a8eff65b969e647f34d647ad56700b796
glowfire <kunchyi1@gmail.com>
2016-07-24 16:56:43 +0800

    Create accessories.js

---
+++ items/shippinglist/accessories.js
@@ -0,0 +1,16 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the fifth section.
+
+registerItem({
+	"name":"",
+	"category":"Accessory",
+	"sell":,
+	"buy":,
+	"ability":{
+		"":
+	},
+	"flavourtext":"A staple food for many. You can toast it, stuff it, stick things between it or eat it any other way you'd like.",
+	"pricemultiplier":"E",
+	"craftinglevel":,
+	"materials":[""]
+})
