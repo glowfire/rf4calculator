cb2d169834e735cba5ac1d7a49d57ad3e5acfdce
glowfire <kunchyi1@gmail.com>
2016-07-24 17:02:03 +0800

    Update accessories.js

--- items/shippinglist/accessories.js
+++ items/shippinglist/accessories.js
@@ -2,15 +2,59 @@
 // This list contains the items listed in the fifth section.
 
 registerItem({
+	"name":"Charm",
+	"sell":45,
+	"buy":350,
+	"ability":{
+		"critresP":100
+	},
+	"flavourtext":"A charm from the Far East. Protects you from misfortune by blocking critical hits.",
+	"craftinglevel":3,
+	"materials":["Cloths and Skins","Strings"],
+	"pricemultiplier":"E",
+	"category":"Accessory"
+})
+
+registerItem({
+	"name":"Work Gloves",
+	"sell":13,
+	"buy":1500,
+	"ability":{
+		"atk":5,
+		"diz":1,
+		"stunP":10
+	},
+	"flavourtext":"Protects your hands from blisters. A must-have for farming.",
+	"craftinglevel":4,
+	"materials":["Cloths and Skins"],
+	"pricemultiplier":"E",
+	"category":"Accessory"
+})
+
+registerItem({
+	"name":"Cheap Bracelet",
+	"sell":24,
+	"buy":100,
+	"ability":{
+		"mdef":5
+	},
+	"flavourtext":"An old, rusty bracelet. Slight raises magic defense.",
+	"craftinglevel":5,
+	"materials":["Minerals"],
+	"pricemultiplier":"E",
+	"category":"Accessory"
+})
+
+registerItem({
 	"name":"",
-	"category":"Accessory",
 	"sell":,
 	"buy":,
 	"ability":{
 		"":
 	},
-	"flavourtext":"A staple food for many. You can toast it, stuff it, stick things between it or eat it any other way you'd like.",
-	"pricemultiplier":"E",
+	"flavourtext":".",
 	"craftinglevel":,
-	"materials":[""]
+	"materials":[""],
+	"pricemultiplier":"E",
+	"category":"Accessory"
 })
