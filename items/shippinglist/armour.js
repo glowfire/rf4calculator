ef2f4f187b8079d43eb5629d8b7f0902cbc4f1e7
glowfire <kunchyi1@gmail.com>
2016-07-30 21:30:53 +0800

    Update and rename wardrobe.js to armour.js

---
+++ items/shippinglist/armour.js
@@ -0,0 +1,296 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the fourth section.
+
+registerItem({
+	"name":"Four Dragons' Vest",
+	"sell":12e4,
+	"ability":{
+		"def":3600,
+		"mdef":3260,
+		"fireresP":5,
+		"waterresP":5,
+		"earthresP":5,
+		"windresP":5
+	},
+	"flavourtext":"A vest blessed by Native Dragons. Can tolerate any attack.",
+	"craftinglevel":,
+	"materials":["","",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Royal Garter",
+	"sell":48e3,
+	"ability":{
+		"def":2824,
+		"mdef":2780
+	},
+	"flavourtext":"Makes you feel the dignity of a king.",
+	"craftinglevel":,
+	"materials":["","",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Rune Vest",
+	"sell":3e4,
+	"ability":{
+		"def":2260,
+		"mdef":2310
+	},
+	"flavourtext":"A vest imbued with the power of runes.",
+	"craftinglevel":,
+	"materials":[""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Dragon Cloak",
+	"sell":7800,
+	"ability":{
+		"def":1250,
+		"mdef":1200
+	},
+	"flavourtext":"A cloak with the symbol of a dragon. Anything with dragons on it is strong, right?",
+	"craftinglevel":,
+	"materials":[""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Power Protector",
+	"sell":18e3,
+	"ability":{
+		"def":1890,
+		"mdef":1820
+	},
+	"flavourtext":"A reinforced protector. It will prevent any accidents.<br>Disclaimer: Not to be used when cooking.",
+	"craftinglevel":,
+	"materials":[""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Elvish Cloak",
+	"sell":6400,
+	"ability":{
+		"def":888,
+		"mdef":850
+	},
+	"flavourtext":"A brilliant cloak that is absolutely perfect. Only the beautiful can wear this.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Lamellar Vest",
+	"sell":4500,
+	"ability":{
+		"def":254,
+		"mdef":140
+	},
+	"flavourtext":"Armor made from plate-shaped scales. It doesn't impede your speed and also protects you well.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Platinum Mail",
+	"sell":3800,
+	"buy":198e3,
+	"ability":{
+		"def":152,
+		"mdef":80
+	},
+	"flavourtext":"A luxurious mail made of platinum.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Sparkling Shirt",
+	"sell":600,
+	"buy":49200,
+	"ability":{
+		"def":78,
+		"mdef":52
+	},
+	"flavourtext":"This is the biggest challenge for people who love clothes. Wearing something like this... Are you out of mind?",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Wind Cloak",
+	"sell":980,
+	"buy":72780,
+	"ability":{
+		"def":92,
+		"mdef":60,
+		"windresP":10
+	},
+	"flavourtext":"A cloak imbued with the element of wind. Don't get any bright ideas and start jumping off of buildings, though.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Protector",
+	"sell":1e3,
+	"buy":106200,
+	"ability":{
+		"def":123,
+		"mdef":68
+	},
+	"flavourtext":"Equipment that prevents accidents before they happen.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Scale Vest",
+	"sell":620,
+	"buy":31e3,
+	"ability":{
+		"def":68,
+		"mdef":36
+	},
+	"flavourtext":"A vest studded with scales.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Chain Mail",
+	"sell":420,
+	"buy":18e3,
+	"ability":{
+		"def":50,
+		"mdef":30
+	},
+	"flavourtext":"With this, straw sandals and a ninja sword on your back, you're on your way to becoming a true warrior!",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Cotton Cloth",
+	"sell":125,
+	"buy":4980,
+	"ability":{
+		"def":18,
+		"mdef":11
+	},
+	"flavourtext":"Might look like bandages, but they're not. Also not recommended as a chest wrap for women.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Mail",
+	"sell":206,
+	"buy":9200,
+	"ability":{
+		"def":36,
+		"mdef":18
+	},
+	"flavourtext":"You've got mail!",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Mercenary's Cloak",
+	"sell":3e3,
+	"ability":{
+		"def":385,
+		"mdef":320
+	},
+	"flavourtext":"Makes you look super cool.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Wooly Shirt",
+	"sell":4200,
+	"ability":{
+		"def":578,
+		"mdef":468
+	},
+	"flavourtext":"The puffy wool protects you from attacks, but it's soooooo annoying to wash.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Vest",
+	"sell":20,
+	"buy":1300,
+	"ability":{
+		"def":9,
+		"mdef":2
+	},
+	"flavourtext":"Fancy clothing that keeps you warm. Also called a waistcoat.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"Shirt",
+	"sell":13,
+	"buy":120,
+	"ability":{
+		"def":5
+	},
+	"flavourtext":"A plain shirt to wear under clothes. It's made from materials which absorb sweat well.",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"Armor",
+	"pricemultiplier":"E"
+})
+
+registerItem({
+	"name":"",
+	"sell":,
+	"buy":,
+	"ability":{
+		"":
+	},
+	"flavourtext":".",
+	"craftinglevel":,
+	"materials":[""],
+	"category":"",
+	"pricemultiplier":"E"
+})
