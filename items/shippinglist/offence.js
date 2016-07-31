d88a74630fb3a18d85bfca914172e6a81807ff8d
glowfire <kunchyi1@gmail.com>
2016-07-31 12:10:41 +0800

    Create offence.js

---
+++ items/shippinglist/offence.js
@@ -0,0 +1,16 @@
+// Registration of items in the order as shown in the shipping records.
+// This list contains the items listed in the third section.
+
+registerItem({
+	"name":"",
+	"sell":,
+	"buy":,
+	"ability":{
+		"":
+	},
+	"flavourtext":".",
+	"craftinglevel":,
+	"materials":["",""],
+	"category":"",
+	"pricemultiplier":"E"
+})
