d7c6b8d572b3c80528c9e7dd02bfdae5e7d4126a
glowfire <kunchyi1@gmail.com>
2016-08-09 18:58:43 +0800

    Update lvbased.js

--- scripts/lvbased.js
+++ scripts/lvbased.js
@@ -1,4 +1,25 @@
 function getPriceTable(){
 	var headerArray=["Lv.","Multiplier<br>(Type: ","Sell Price","Increment","Cumulative<br>Increment"];
-	headerArray[1]+=currentItem.pricemultiplier+")"
+	headerArray[1]+=currentItem.pricemultiplier+")";
+	
+	var integerCorrector = 1e8;
+	
+	var lvArray = [];
+	var priceMultiArray = getPriceMultiplierArray(currentItem.pricemultiplier);
+	var sellPriceArray = [];
+	var incrementArray = ["N/A"];
+	var cumulativeArray = [];
+	var cumSum = 0;
+	
+	for (i=0;i<maxLv;i++){
+		lvArray.push(i+1);
+		var multiplier = priceMultiArray[i]*integerCorrector
+		sellPriceArray.push(multiplier*currentItem.sell/integerCorrector)
+		cumulativeArray.push(cumSum)
+		if (i>0){
+			incrementArray.push(sellPriceArray[i]-sellPriceArray[i-1])
+			cumSum+=incrementArray[i];
+		}
+	}
+	
 }
