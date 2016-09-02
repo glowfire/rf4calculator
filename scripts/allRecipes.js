35b7791b7f3c1301d0f4435f60cd4e715f9945b2
glowfire <kunchyi1@gmail.com>
2016-09-02 16:11:46 +0800

    Update allRecipes.js

--- scripts/allRecipes.js
+++ scripts/allRecipes.js
@@ -1,67 +1,8 @@
-var allRecipesListing=[];
 
-var recipeHeaderArray=["Name","Category","Sell Price","Materials"]
-
-for (i=0;i<sortedMasterNameList.length;i++){
-	var currentNameSet = sortedMasterNameList[i];
-	var currentUniqueName = currentNameSet[2];
-	var currentObject = masterItemList[currentUniqueName]
-	if (currentObject.materials!==undefined){
-		var currentRecipe = currentObject;
-		var currentRecipeInfo = [];
-		currentRecipeInfo.push(currentRecipe.name);
-		currentRecipeInfo.push(currentRecipe.category);
-		currentRecipeInfo.push(currentRecipe.sell);
-		//currentRecipeInfo.arrayPush(currentRecipe.materials);
-		allRecipesListing.push(currentRecipeInfo)
-	}
-}
-
-function categoryComparator(a, b) {
-	var categoryColumnIndex = recipeHeaderArray.indexOf("Category")
-	if (a[categoryColumnIndex] < b[categoryColumnIndex]){ return -1;
-	} else if (a[categoryColumnIndex] > b[categoryColumnIndex]){ return 1;
-	} else {return 0};
-}
-
-var sortedRecipeListByCategory = allRecipesListing.sort(categoryComparator);
-
-var allRecipesTable = document.createElement("table");
-//document.getElementById('otherInfo').appendChild(allRecipesTable);
-allRecipesTableHeader=document.createElement('tr');
-allRecipesTable.appendChild(allRecipesTableHeader);
-
-function appendRecipeHeader(string){
-	var c = document.createElement('td');
-	allRecipesTableHeader.appendChild(c);
-	c.innerHTML=string;
-	c.style.color="yellow"
-	if (string=="Materials"){
-		c.colSpan = 6
-	}
-}
-
-recipeHeaderArray.forEach(appendRecipeHeader)
-
-for (i=0;i<allRecipesListing.length;i++){
-	var row = document.createElement('tr');
-	allRecipesTable.appendChild(row);
-	
-	var currentRow = allRecipesListing[i];
-	var nColumn = recipeHeaderArray.length+5
-	for (j=0;j<nColumn;j++){
-		var c = document.createElement('td');
-		row.appendChild(c);
-		var currentString = currentRow[j]
-		if (currentString!==undefined){
-			c.innerHTML=currentString;
-		} else {
-			c.innerHTML=""
-		}
-	}
-}
 
 function showAllRecipes(){
 	clearPage()
-	document.getElementById('otherInfo').appendChild(allRecipesTable);
+	var p=document.createElement('p')
+	p.innerHTML="Coming Soon"
+	document.getElementById('otherInfo').appendChild(p);
 }
