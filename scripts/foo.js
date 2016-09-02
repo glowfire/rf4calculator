febdd5cc6b39f3d7d92cb7cdbe9b46266f18d963
glowfire <kunchyi1@gmail.com>
2016-09-02 17:12:16 +0800

    Update foo.js

--- scripts/foo.js
+++ scripts/foo.js
@@ -1,6 +1,51 @@
+var allRecipesListing = []
+var allRecipeTableHeaderArray = ["Name","Category","Sell Price"]
+
+for (i=0;i<sortedMasterNameList.length;i++){
+	var currentNameSet = sortedMasterNameList[i];
+	var currentUniqueName = currentNameSet[2];
+	var currentObject = masterItemList[currentUniqueName]
+	if (currentObject.materials!==undefined){
+		var currentRecipe = currentObject;
+		var currentRecipeInfo = [];
+		currentRecipeInfo.push(currentRecipe.name)
+		currentRecipeInfo.push(currentRecipe.category)
+		currentRecipeInfo.push(currentRecipe.sell)
+		allRecipesListing.push(currentRecipeInfo)
+	}
+}
+
+var recipeTable = document.createElement("table");
+//document.getElementById('otherInfo').appendChild(seedTable);
+recipeTableHeader=document.createElement('tr');
+recipeTable.appendChild(recipeTableHeader);
+
+function appendRecipeHeader(string){
+	var c = document.createElement('td');
+	recipeTableHeader.appendChild(c);
+	c.innerHTML=string;
+	c.style.color="yellow"
+}
+
+allRecipeTableHeaderArray.forEach(appendSeedHeader)
+
+for (i=0;i<allRecipesListing.length;i++){
+	var row = document.createElement('tr');
+	recipeTable.appendChild(row);
+	
+	var currentRow = allRecipesListing[i];
+	var nColumn=currentRow.length-1
+	for (j=0;j<nColumn;j++){
+		var c = document.createElement('td');
+		row.appendChild(c);
+		c.innerHTML=currentRow[j];
+	}
+}
+
 function showAllRecipes(){
 	clearPage()
 	var p=document.createElement('p')
 	p.innerHTML="Coming Soon!"
-	document.getElementById('otherInfo').appendChild(p);
+	//document.getElementById('otherInfo').appendChild(p);
+	document.getElementById('otherInfo').appendChild(recipeTable);
 }
